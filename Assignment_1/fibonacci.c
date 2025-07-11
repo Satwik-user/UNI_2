dac80023bced78299c949752e4116df2c89f0da8
Satwik-user <satwik2400@gmail.com>
2025-07-11 15:46:50 +0000

    Jul 11, 2025, 3:46 PM

---
+++ Assignment_1/fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n,a=0,b=1,c;
+    printf("Enter no of terms:");
+    scanf("%d",&n);
+    printf("%d %d ",a,b);
+    for(int i=0;i<n-2;i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    
+    return 0;
+}
