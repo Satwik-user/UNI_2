ddf6311b36c304859f6e201289ffb575b8b6369b
Satwik-user <satwik2400@gmail.com>
2025-07-12 07:33:33 +0000

    Jul 12, 2025, 7:33 AM

--- Assignment_1/del_ele.py
+++ Assignment_1/del_ele.py
@@ -2,3 +2,5 @@ n=int(input("Enter size of array:"))
 arr=[]
 for i in range(n):
     arr.append(int(input("Enter element:")))
+pos=int(input("Enter position of element to be deleted:"))
+del(arr[pos])
