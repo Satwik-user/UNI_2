f0f60e1496b6b79ccb9f1965cd2c311309baa15d
Satwik-user <satwik2400@gmail.com>
2025-07-12 17:58:57 +0000

    Jul 12, 2025, 5:58 PM

--- Assignment_1/duplicate.c
+++ Assignment_1/duplicate.c
@@ -12,7 +12,14 @@ int main()
     }
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n)
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            for(int k=j;k<n-1;k++)
+            {
+                
+            }
+        }
     }
     
     return 0;
