9f1c43b84a5bc6611ccdfdc2573526a44600d704
Satwik-user <satwik2400@gmail.com>
2025-07-12 03:54:06 +0000

    Jul 12, 2025, 3:54 AM

--- Assignment_1/del_ele.c
+++ Assignment_1/del_ele.c
@@ -13,9 +13,9 @@ int main()
     int del_pos;
     pritnf("Enter position of element to be deleted:");
     scanf("%d",&del_pos);
-    for(int i=0;i<n;i++)
+    for(int i=del;i<n;i++)
     {
-        
+
     }
     
     return 0;
