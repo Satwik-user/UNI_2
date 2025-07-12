3a0470b35dd2d06bcfa0e3a9e72a86a61f7468ea
Satwik-user <satwik2400@gmail.com>
2025-07-12 18:09:33 +0000

    Jul 12, 2025, 6:09 PM

--- Assignment_1/max_min.c
+++ Assignment_1/max_min.c
@@ -5,7 +5,11 @@ int main()
     printf("Enter size of array:");
     scanf("%d",&n);
     int arr[n];
-    
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter element:");
+        scanf("%d",&arr[i]);
+    }
     
     return 0;
 }
