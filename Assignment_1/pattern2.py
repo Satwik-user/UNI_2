c7be149f38216204aa3a9e559a92637073815597
Satwik-user <satwik2400@gmail.com>
2025-07-12 05:41:20 +0000

    Jul 12, 2025, 5:41 AM

--- Assignment_1/pattern2.py
+++ Assignment_1/pattern2.py
@@ -10,7 +10,7 @@ for i in range(n,0,-1):
 
 for i in range(n,0,-1):
     for j in range(0,n-i+2):
-        print(' ')
+        print(' ',end='')
     for k in range(n,n-i+3,-1):
         print(k)
     for l in range(n-i+3,n+1):
