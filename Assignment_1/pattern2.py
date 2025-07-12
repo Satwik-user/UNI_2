53dcead52f0c95d48706c099336945858e786bbd
Satwik-user <satwik2400@gmail.com>
2025-07-12 05:40:09 +0000

    Jul 12, 2025, 5:40 AM

--- Assignment_1/pattern2.py
+++ Assignment_1/pattern2.py
@@ -2,3 +2,17 @@ n=int(input("Enter no of lines:"))
 for i in range(n,0,-1):
     for j in range(1,i):
         print(' ')
+    for k in range(n,i-1,-1):
+        print(k)
+    for l in range(i+1,n+1):
+        print(l)
+    print()
+
+for i in range(n,0,-1):
+    for j in range(0,n-i+2):
+        print(' ')
+    for k in range(n,n-i+3,-1):
+        print(k)
+    for l in range(n-i+3,n+1):
+        print(l)
+    print()
