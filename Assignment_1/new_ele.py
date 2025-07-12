836dfa7d8f14a322c98c4030576b301a09cbce8a
Satwik-user <satwik2400@gmail.com>
2025-07-12 07:28:29 +0000

    Jul 12, 2025, 7:28 AM

--- Assignment_1/new_ele.py
+++ Assignment_1/new_ele.py
@@ -2,5 +2,5 @@ n=int(input("Enter size of array:"))
 arr=[]
 for i in range(n):
     arr.append(int(input("Enter element:")))
-ele=int(input("Enter nerelement:"))
-pos=int()
+ele=int(input("Enter new element:"))
+pos=int(input("Enter p"))
