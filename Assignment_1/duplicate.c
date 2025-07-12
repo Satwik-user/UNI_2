a13514b6dfb988ebe47891b9057fee66208bf9d3
Satwik-user <satwik2400@gmail.com>
2025-07-12 17:59:43 +0000

    Jul 12, 2025, 5:59 PM

--- Assignment_1/duplicate.c
+++ Assignment_1/duplicate.c
@@ -15,6 +15,7 @@ int main()
         for(int j=i+1;j<n;j++)
         {
             if(arr[i]==arr[j])
+            {
             for(int k=j;k<n-1;k++)
             {
                 arr[k]=arr[k+1];
