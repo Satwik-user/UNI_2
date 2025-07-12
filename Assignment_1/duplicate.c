acf4d4f90c405a06426e0d45747284be3e313365
Satwik-user <satwik2400@gmail.com>
2025-07-12 17:34:43 +0000

    Jul 12, 2025, 5:34 PM

---
+++ Assignment_1/duplicate.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter size ofo array:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter element:");
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;)
+    }
+    
+    return 0;
+}
