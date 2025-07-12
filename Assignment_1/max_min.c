237a2f7f2972b9b93c4d035409ce5d6437b4f9d5
Satwik-user <satwik2400@gmail.com>
2025-07-12 18:18:28 +0000

    Jul 12, 2025, 6:18 PM

--- Assignment_1/max_min.c
+++ Assignment_1/max_min.c
@@ -10,10 +10,13 @@ int main()
         printf("Enter element:");
         scanf("%d",&arr[i]);
     }
-    int max=arr[0], miin=arr[0];
+    int max=arr[0], min=arr[0];
     for(int i=0;i<n;i++)
     {
-        
+        if(arr[i]>max)
+        max=arr[i];
+        if(arr[i]<min)
+        min=arr[i];
     }
     
     return 0;
