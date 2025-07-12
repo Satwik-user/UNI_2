892ae9abddcd183e3717ff18800791c359c83df8
Satwik-user <satwik2400@gmail.com>
2025-07-12 03:50:34 +0000

    Jul 12, 2025, 3:50 AM

---
+++ Assignment_1/new_ele.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter element:");
+        scanf("%d",&arr[i]);
+    }
+    int ele,pos;
+    printf("Enter element and position:");
+    scanf("%d %d",&ele,&pos);
+    arr[pos]=ele;
+    printf("Modified array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
