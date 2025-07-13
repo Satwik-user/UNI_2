0e7d1ea74ef232c8d7dd20cf7ffc3f34ad437dca
Satwik-user <satwik2400@gmail.com>
2025-07-13 15:58:25 +0000

    Jul 13, 2025, 3:58 PM

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-c=int(input("Enter third number:"))
-if a>b and a>c:
-    print("Largest:",a)
-elif b>a and b>c:
-    print("Largest:",b)
-elif c>a and c>b:
-    print("Largest:",c)
+n=int(input("Enter a number:"))
