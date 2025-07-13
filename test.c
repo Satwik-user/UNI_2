931b6443284e4fcaeefbfd3094c2edcd96b71a97
Satwik-user <satwik2400@gmail.com>
2025-07-13 18:01:21 +0000

    Jul 13, 2025, 6:01 PM

--- test.c
+++ test.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 int main()
 {
-    int n,c=0;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        c++;
-    }
-    if(c==2)
-    printf("Prime number\n");
-    else
-    printf("Composite number\n");
-    
     return 0;
 }
