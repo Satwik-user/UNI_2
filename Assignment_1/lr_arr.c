9d43e1d5de3df49d73308c2c37c7067ff6664ed3
Satwik-user <satwik2400@gmail.com>
2025-07-13 04:30:02 +0000

    Jul 13, 2025, 4:30 AM

--- Assignment_1/lr_arr.c
+++ Assignment_1/lr_arr.c
@@ -19,8 +19,8 @@ int main()
         for(int j=0;j<n-1;j++)
         {
             arr[j]=arr[j+1];
-            arr[n-1]=temp;
         }
+        arr[n-1]=temp;
         k--;
     }
     printf("Left rotation array:");
@@ -34,8 +34,8 @@ int main()
         for(int j=0;j<n-1;j++)
         {
             arr[j]=arr[j+1];
-            arr[0]=temp;
         }
+        arr[0]=temp;
         k--;
     }
     
