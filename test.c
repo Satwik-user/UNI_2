7485adf41f45c08ec6ae27bfa8f4690ea8cf9a5d
Satwik-user <satwik2400@gmail.com>
2025-07-13 18:03:25 +0000

    Jul 13, 2025, 6:03 PM

--- test.c
+++ test.c
@@ -7,9 +7,10 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
     {
-        printf("enter element:");
+        printf("Enter element:");
         scanf("%d",&arr[i]);
     }
+    
     
     return 0;
 }
