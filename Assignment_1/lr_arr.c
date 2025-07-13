68f0dc3246dad5a4087a90c5ee42471e109a8b79
Satwik-user <satwik2400@gmail.com>
2025-07-13 04:27:47 +0000

    Jul 13, 2025, 4:27 AM

--- Assignment_1/lr_arr.c
+++ Assignment_1/lr_arr.c
@@ -28,7 +28,16 @@ int main()
     {
         printf("%d ",&arr[i]);
     }
-    
+    while(k>0)
+    {
+        int temp=arr[0];
+        for(int j=0;j<n-1;j++)
+        {
+            arr[j]=arr[j+1];
+        }
+        arr[n-1]=temp;
+        k--;
+    }
     
     return 0;
 }
