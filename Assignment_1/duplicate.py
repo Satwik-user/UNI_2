19f8bf23be649b4683401064d181e9668c0fed5a
Satwik-user <satwik2400@gmail.com>
2025-07-13 06:01:58 +0000

    Jul 13, 2025, 6:01 AM

--- Assignment_1/duplicate.py
+++ Assignment_1/duplicate.py
@@ -1,13 +1,11 @@
 n=int(input("Enter size of array:"))
 arr=[]
+res=[]
 for i in range(n):
     arr.append(int(input("Enter element:")))
-for i in range(n):
-    for j in range(i+1,n):
-        if arr[i]==arr[j]:
-            del(arr[j])
-            n-=1
-            j-=1
+for item in arr:
+    if item not in res:
+        res[]
 print("Modified array:",end='')
 for i in range(n):
     print(arr[i],end=' ')
