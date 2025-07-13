17d008db4169c9de180b53d03d3345324c7b8885
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:50:28 +0000

    Jul 13, 2025, 5:50 PM

--- test.c
+++ test.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-int main()
-{
-    int n,a=0,b=1,c;
-    printf("Enter number of terms:");
-    scanf("%d",&n);
-    printf("%d %d ",a,b);
-    for(int i=0;i<n-2;i++)
-    {
-        c=a+b;
-        printf("%d ",c);
-        a=b;
-        b=c;
-    }
-    printf("\n");
-    
-    return 0;
-}
+#include <stdio.>
