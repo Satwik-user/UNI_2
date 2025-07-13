37d61c3c76ce6fc97856ad1cf5d9e5ac986aaa3d
Satwik-user <satwik2400@gmail.com>
2025-07-13 06:02:03 +0000

    Jul 13, 2025, 6:02 AM

--- Assignment_1/duplicate.py
+++ Assignment_1/duplicate.py
@@ -5,7 +5,7 @@ for i in range(n):
     arr.append(int(input("Enter element:")))
 for item in arr:
     if item not in res:
-        res[]
+        res.append
 print("Modified array:",end='')
 for i in range(n):
     print(arr[i],end=' ')
