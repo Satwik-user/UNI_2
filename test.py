915d886bc07fd0b199acc8ee9500c1b912448311
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:34:18 +0000

    Jul 13, 2025, 5:34 PM

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
 n=int(input("Enter a number:"))
-cpy=n
-rev=0
-while n>0:
-    rev=rev*10+(n%10)
-    n//=10
-if rev==cpy:
-    print("Palindrome number")
-else:
-    print("Not a palindrome number")
