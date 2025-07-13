3dab916c83b16c305022fc4885466fa7d33acb35
Satwik-user <satwik2400@gmail.com>
2025-07-13 15:57:16 +0000

    Jul 13, 2025, 3:57 PM

--- test.c
+++ test.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n,rev
+    int n,rev=0;
     printf("Enter a number:");
     scanf("%d",&n);
     while(n>0)
     {
         rev=rev*10+(n%10);
+        
     }
     
     return 0;
