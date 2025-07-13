0e3fa1ce3078079844723c8a57b6d683e44a6a0e
Satwik-user <satwik2400@gmail.com>
2025-07-13 13:12:44 +0000

    Jul 13, 2025, 1:12 PM

--- Assignment_1/lr_arr.py
+++ Assignment_1/lr_arr.py
@@ -7,10 +7,10 @@ cpy_k=k
 l_arr=arr[:]
 r_arr=arr[:]
 while k>0:
-    l_arr[1:]+l_arr[0]
+    l_arr=l_arr[1:]+l_arr[0]
     k-=1
 while cpy_k>0:
-    r_arr[n-1]+r_arr[0:n-2]
+    r_arr=r_arr[n-1]+r_arr[0:n-2]
     cpy_k-=1
 print("Left rotation array:",l_arr)
 print("Right rotation array:",r_arr)
