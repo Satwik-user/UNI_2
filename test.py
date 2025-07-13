9e0f12834c7321a975d7b0dae87a47b4de60d3ec
Satwik-user <satwik2400@gmail.com>
2025-07-13 18:04:46 +0000

    Jul 13, 2025, 6:04 PM

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-n=int(input("Enter a number:"))
-c=0
-for i in range(1,n+1):
-    if n%i==0:
-        c+=1
-if c==2:
-    print("Prime Number")
-else:
-    print("Composite number")
+n=int(input("Enter "))
