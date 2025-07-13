773a19b889bf9fb67d3f9c25c1062a52dc88294d
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:39:37 +0000

    Jul 13, 2025, 5:39 PM

--- test.c
+++ test.c
@@ -1,14 +1,1 @@
 #include <stdio.h>
-int main()
-{
-    int n,fact=1;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-    {
-        fact*=i;
-    }
-    printf("Factorial of the number:%d\n",fact);
-    
-    return 0;
-}
