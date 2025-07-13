39996f58848c3fa4988adc89d7b9c42770b34aa9
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:54:04 +0000

    Jul 13, 2025, 5:54 PM

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-n=int(input("Enter number of terms:"))
-a=0
-b=1
-print(a,b,end=' ')
-for i in range(n-2):
-    c=a+b
-    print(c,end=' ')
-    a=b
-    b=c
-print()
+n=int(input("Enter a number:"))
