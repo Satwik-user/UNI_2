9601b36d8121d94a36bb8396b5a92cc1165b780b
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:24:51 +0000

    Jul 13, 2025, 5:24 PM

--- test.c
+++ test.c
@@ -4,12 +4,16 @@ int main()
     int n,rev=0;
     printf("Enter a number:");
     scanf("%d",&n);
+    int cpy=n;
     while(n>0)
     {
         rev=rev*10+(n%10);
         n/=10;
     }
-    printf("Reverse of number:%d\n",rev);
+    if(rev==cpy)
+    printf("Palindrome number:\n");
+    else
+    printf("Not a palindrome number:\n");
     
     return 0;
 }
