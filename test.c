7eb39e1da2a15788f032bebcd23089e20dbfd3a2
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:52:16 +0000

    Jul 13, 2025, 5:52 PM

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n,c=0;
     printf("Enter a number");
     scanf("%d",&n);
-    for(int i=0;i<=n)
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c++;
+    }
     
     return 0;
 }
