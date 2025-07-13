46ecb66d3e58c16869c7478d1524b29ba56dc630
Satwik-user <satwik2400@gmail.com>
2025-07-13 13:15:14 +0000

    Jul 13, 2025, 1:15 PM

--- Assignment_1/lr_arr.py
+++ Assignment_1/lr_arr.py
@@ -3,14 +3,13 @@ arr=[]
 for i in range(n):
     arr.append(int(input("Enter element:")))
 k=int(input("Enter value of k:"))
-cpy_k=k
 l_arr=arr[:]
 r_arr=arr[:]
-while k>0:
-    l_arr=l_arr[1:]+l_arr[0]
+for _ in range(k):
+    l_arr=l_arr[1:]+[l_arr[0]]
     k-=1
-while cpy_k>0:
-    r_arr=r_arr[n-1]+r_arr[0:n-2]
+for _ in range(k):
+    r_arr=[r_arr[-1]]+r_arr[:-1]
     cpy_k-=1
 print("Left rotation array:",l_arr)
 print("Right rotation array:",r_arr)
