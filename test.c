9972aaa16f422183379cbd773a05490386d7c9ac
Satwik-user <satwik2400@gmail.com>
2025-07-13 17:31:53 +0000

    Jul 13, 2025, 5:31 PM

--- test.c
+++ test.c
@@ -1,19 +1,1 @@
 #include <stdio.h>
-int main()
-{
-    int n,rev=0;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    int cpy=n;
-    while(n>0)
-    {
-        rev=rev*10+(n%10);
-        n/=10;
-    }
-    if(rev==cpy)
-    printf("Palindrome number\n");
-    else
-    printf("Not a palindrome number\n");
-    
-    return 0;
-}
