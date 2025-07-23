f9686c000715a9f4f50a3951489fefd97f009c62
Satwik-user <satwik2400@gmail.com>
2025-07-23 16:17:43 +0000

    Jul 23, 2025, 4:17 PM

--- Assignment_2/simult.py
+++ Assignment_2/simult.py
@@ -2,4 +2,7 @@ n1=int(input("Enetr size of list1:"))
 l1=[]
 for i in range(n1):
     l1.append(int(input("Enter element for list1:")))
-n2=int(input("Enter "))
+n2=int(input("Enter size of list2:"))
+l2=[]
+for i in range(n2):
+    
