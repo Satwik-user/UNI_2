e03f28d94211deb75bf174967fb693a18a5da042
Satwik-user <satwik2400@gmail.com>
2025-07-23 15:57:17 +0000

    Jul 23, 2025, 3:57 PM

--- Assignment_2/square.py
+++ Assignment_2/square.py
@@ -2,6 +2,6 @@ n=int(input("Enter size of list:"))
 l=[]
 for i in range(n):
     l.append(int(input("Enter element:")))
-for i in l:
-    i=i*i
+for i in range(n):
+    l[i]
 print("Squared list:",l)
