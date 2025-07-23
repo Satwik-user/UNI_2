4503d3a6c9fbdcd24ed7a7181331038cb72482de
Satwik-user <satwik2400@gmail.com>
2025-07-23 16:10:59 +0000

    Jul 23, 2025, 4:10 PM

--- Assignment_2/concat_2.py
+++ Assignment_2/concat_2.py
@@ -6,6 +6,5 @@ n2=int(input("Enter size of list2:"))
 l2=[]
 for i in range(n2):
     l2.append(int(input("Enter element of list2:")))
-for i in range(n2):
-    l1.append(l2[i])
+concat=
 print("Concatenated list:",l1)
