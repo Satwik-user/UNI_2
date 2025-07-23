747fff29201eaf869ebbf18182a4f37d7c292fcd
Satwik-user <satwik2400@gmail.com>
2025-07-23 16:38:12 +0000

    Jul 23, 2025, 4:38 PM

--- Assignment_2/add_to_list.py
+++ Assignment_2/add_to_list.py
@@ -2,4 +2,4 @@ n=int(input("Enter size of list:"))
 l=[]
 for i in range(n):
     l.append(int(input("Enter element:")))
-index=int(input("Enter index to add "))
+index=int(input("Enter index to add new elemnt next to:"))
