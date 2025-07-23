5032cda20c53a42527cb3fffc759ffab5071ca9a
Satwik-user <satwik2400@gmail.com>
2025-07-23 15:16:41 +0000

    Jul 23, 2025, 3:16 PM

--- Assignment_2/concat.py
+++ Assignment_2/concat.py
@@ -1,4 +1,4 @@
 n1=int(input("Enter size of list1:"))
 n2=int(input("Enter size of list2:"))
 for i in range(n1):
-    l1.append(int(append("Enter element")))
+    l1.append(int(append("Enter element of list1:")))
