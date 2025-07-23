8e74c340eb3bb19a324564d3c51259ab110b5284
Satwik-user <satwik2400@gmail.com>
2025-07-23 15:13:48 +0000

    Jul 23, 2025, 3:13 PM

--- Assignment_2/rev.py
+++ Assignment_2/rev.py
@@ -2,6 +2,6 @@ n=int(input("Enter size of list:"))
 l=[]
 for i in range(n):
     l.append(int(input("Enter element:")))
-print("Original List:")
+print("Original List:",l)
 l.reverse()
 print("Reverse of the list:",l)
