7e35850dab7072b58a30681317e6c342cea19793
Satwik-user <satwik2400@gmail.com>
2025-07-23 16:35:24 +0000

    Jul 23, 2025, 4:35 PM

--- Assignment_2/str_list.py
+++ Assignment_2/str_list.py
@@ -2,5 +2,5 @@ n=int(input("Enter size of list:"))
 l=[]
 for i in range(n):
     l.append(input("Enter string element:"))
-l=[string for string in l if l]
+l=[string for string in l if len(string)>0]
 print("Modified List:",l)
