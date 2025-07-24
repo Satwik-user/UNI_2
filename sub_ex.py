41e798b5603f0e851def2ac30667a43a85888e7e
Satwik-user <satwik2400@gmail.com>
2025-07-24 03:27:58 +0000

    Jul 24, 2025, 3:27 AM

--- sub_ex.py
+++ sub_ex.py
@@ -1,2 +1,7 @@
-n1=int(input("Enter size of sub list:"))
-n2=int(input("Enter size of nestef=d"))
+n1=int(input("Enter size of nested list:"))
+n2=int(input("Enter size of sub list:"))
+l1=[]
+for i in range(n1):
+    l2=[]
+    for j in range(n2):
+        l2.append(int(input("")))
