037e1f75695c4144d0071659d60ce652931fbff2
Satwik-user <satwik2400@gmail.com>
2025-07-24 09:04:28 +0000

    Jul 24, 2025, 9:04 AM

--- Assignment_2/replace.py
+++ Assignment_2/replace.py
@@ -2,4 +2,5 @@ n=int(input("Enter size of list:"))
 l=[]
 for i in range(n):
     l.append(int(input("Enter element:")))
-old_ele=int(input("Enter "))
+old_ele=int(input("Enter value to be replaced:"))
+new_ele=int()
