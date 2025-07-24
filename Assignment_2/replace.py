c9e672751e9a5ff6dd96c7275d8c0307a731c1d7
Satwik-user <satwik2400@gmail.com>
2025-07-24 09:04:40 +0000

    Jul 24, 2025, 9:04 AM

--- Assignment_2/replace.py
+++ Assignment_2/replace.py
@@ -3,4 +3,4 @@ l=[]
 for i in range(n):
     l.append(int(input("Enter element:")))
 old_ele=int(input("Enter value to be replaced:"))
-new_ele=int(input("Enter "))
+new_ele=int(input("Enter new value"))
