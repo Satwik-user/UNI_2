58db11cb7ed2d210114c91aea83d27e32c7c3d38
Satwik-user <satwik2400@gmail.com>
2025-07-31 10:25:56 +0000

    Jul 31, 2025, 10:25 AM

--- DSA/Assignment_2/dictionary/check_val.py
+++ DSA/Assignment_2/dictionary/check_val.py
@@ -1,6 +1,11 @@
 dict0={1:'Satwik',2:'Rahul',3:'Rohan'}
-val='Shyam'
+val='Rohan'
+f=0
 for i in dict0:
     if val in dict0.values():
-        print("Value found!")
+        f=1
         break
+if f==1:
+    print("Value found!")
+else:
+    print("Value not found")
