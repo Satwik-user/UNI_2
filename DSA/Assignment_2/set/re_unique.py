52a8053da354e3ba56c3822c01d620840dc223d9
Satwik-user <satwik2400@gmail.com>
2025-07-31 09:06:15 +0000

    Jul 31, 2025, 9:06 AM

--- DSA/Assignment_2/set/re_unique.py
+++ DSA/Assignment_2/set/re_unique.py
@@ -1,8 +1,4 @@
-set1=(1,2,3,4,5)
-set2=(2,3,4,5,6)
-set3=set()
-for i in set1:
-    for j in set2:
-        if i!=j:
-            set3.add(i)
+set1={1,2,3,4,5}
+set2={2,3,4,5,6}
+set3=set1^set2
 print(set3)
