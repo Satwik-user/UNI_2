0c8fa324b5d33f15fb38d9fa8844cc1136202e11
Satwik-user <satwik2400@gmail.com>
2025-07-31 09:54:00 +0000

    Jul 31, 2025, 9:54 AM

--- DSA/Assignment_2/dictionary/convert_list.py
+++ DSA/Assignment_2/dictionary/convert_list.py
@@ -1,6 +1,4 @@
 list1=[1,2,3,4,5]
-list2=[6,7,8,9,10]
-dict1=dict(list1)
-dict2=dict(list2)
-print(dict1)
-print(dict2)
+list2=['Satwik','Sohaum','Rahul','Rohan']
+dict0=dict(zip(list1,list2))
+print(dict0)
