de2b4cf40930b11f61a82d5e4e329e33bedb0587
Satwik-user <satwik2400@gmail.com>
2025-08-03 05:56:24 +0000

    Aug 3, 2025, 5:56 AM

--- DSA/Assignment_2/dictionary/rename.py
+++ DSA/Assignment_2/dictionary/rename.py
@@ -2,6 +2,5 @@ dict0={1:'Satwik',2:'Rohan',3:'Rahul',4:'Shyam'}
 old_key=1
 new_key=0
 if old_key in dict0:
-    dict0[new_key]=dict0[old_key]
-    del dict0[old_key]
+    dict0[new_key]=dict0.pop
 print("Updated Dictionary:",dict0)
