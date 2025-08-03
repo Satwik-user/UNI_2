2100e9e3298633f2bb2e205e45f9b389deedafe4
Satwik-user <satwik2400@gmail.com>
2025-08-03 16:24:55 +0000

    Aug 3, 2025, 4:24 PM

--- test.c
+++ test.c
@@ -1,26 +1,7 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
-    int c=0,n,res=0;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    int cpy=n;
-    while(n>0)
-    {
-        n/=10;
-        c++;
-    }
-    n=cpy;
-    while(n>0)
-    {
-        res+=pow(n%10,c);
-        n/=10;
-    }
-    if(res==cpy)
-    printf("Armstrong number\n");
-    else
-    printf("Not a armstrong number\n");
+    
     
     return 0;
 }
