20feadb086e95a6244eed32a6ede8bbc20d60f84
Satwik-user <satwik2400@gmail.com>
2025-08-03 16:32:45 +0000

    Aug 3, 2025, 4:32 PM

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
 n=int(input("Enter a number:"))
-c=len(str(n))
-cpy=n
-res=0
-while n>0:
-    res+=(n%10)**c
-    n//=10
-if cpy==res:
-    print("Armstrong number")
-else:
-    print("Not a armstrong number")
