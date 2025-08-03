d1d9b322f93e592832796372fd6d65e493b72593
Satwik-user <satwik2400@gmail.com>
2025-08-03 05:45:13 +0000

    Aug 3, 2025, 5:45 AM

--- DSA/Assignment_2/dictionary/del_keys.py
+++ DSA/Assignment_2/dictionary/del_keys.py
@@ -1,6 +1,6 @@
 dict0={'a':1,'b':2,'c':3,'d':4,'e':5}
 del_keys=['a','c','e']
-print("Original Dictionary:")
+print("Original Dictionary:",dict0)
 for key in del_keys:
     if key in dict0:
         del dict0[key]
