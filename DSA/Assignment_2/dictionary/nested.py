0095dbef3cd5691605cbc22ac09273a60111e265
Satwik-user <satwik2400@gmail.com>
2025-08-03 06:55:20 +0000

    Aug 3, 2025, 6:55 AM

---
+++ DSA/Assignment_2/dictionary/nested.py
@@ -0,0 +1,14 @@
+person = {
+    'name': 'John',
+    'age': 30,
+    'address': {
+        'street': '123 Main St',
+        'city': 'Boston',
+        'state': 'MA',
+        'zip': '02101'
+    },
+    'skills': {
+        'programming': ['Python', 'JavaScript', 'C++'],
+        'languages': ['English', 'Spanish']
+    }
+}
