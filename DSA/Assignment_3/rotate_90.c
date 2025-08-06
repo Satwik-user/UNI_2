e09f0e4d403b859a52be74935fdffedec3a0a44c
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:20:07 +0000

    Aug 6, 2025, 4:20 PM

--- DSA/Assignment_3/rotate_90.c
+++ DSA/Assignment_3/rotate_90.c
@@ -23,7 +23,13 @@ int main()
     }
 
     printf("Rotated matrix:\n");
-    for(int i=0;i<m)
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
 
     return 0;
 }
