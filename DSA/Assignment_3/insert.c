cfc48a702052a756c9c19303b5ac61c0fdd13055
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:38:48 +0000

    Aug 6, 2025, 4:38 PM

--- DSA/Assignment_3/insert.c
+++ DSA/Assignment_3/insert.c
@@ -2,13 +2,18 @@
 int main()
 {
     int m,n;
+    printf("Enter row and column size:");
+    scnaf("%d %d",&m,&n);
+    int arr[m][n];
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
         {
-            
+            printf("Enter element:");
+            scanf("%d",&arr[i][j]);
         }
     }
+    
     
     return 0;
 }
