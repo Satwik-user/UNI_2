b6b2d8c61e01f4044ee8c44e9ccd0a05b7ff1e0b
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:03:32 +0000

    Aug 6, 2025, 4:03 PM

--- DSA/Assignment_3/spiral.c
+++ DSA/Assignment_3/spiral.c
@@ -8,11 +8,11 @@ int main()
     
     int row_start=0,row_end=m-1;
     int col_start=0,col_end=n-1;
-    int num=0;
+    int num=1;
     while(row_start<=row_end && col_start<=col_end)
     {
         for(int i=col_start;i<=col_end;i++)
-        arr[row_start][i]=++num;
+        arr[row_start][i]=num;
         row_start++;
         for(int i=row_start;i<=row_end;i++)
         arr[i][col_start]=++num;
