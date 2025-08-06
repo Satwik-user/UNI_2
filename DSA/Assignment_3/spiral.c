fa00a2d1c02298f064605943b72d3460537ae4ef
Satwik-user <satwik2400@gmail.com>
2025-08-06 14:49:03 +0000

    Aug 6, 2025, 2:49 PM

--- DSA/Assignment_3/spiral.c
+++ DSA/Assignment_3/spiral.c
@@ -5,14 +5,7 @@ int main()
     printf("Enter row and column size:");
     scanf("%d %d",&m,&n);
     int arr[m][n];
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            printf("Enter element:");
-            scanf("%d",&arr[i][j]);
-        }
-    }
+    
     
     return 0;
 }
