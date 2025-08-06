7928b01c60756a61838d4e1ffdf5744fc01df185
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:06:49 +0000

    Aug 6, 2025, 4:06 PM

--- DSA/Assignment_3/spiral.c
+++ DSA/Assignment_3/spiral.c
@@ -12,7 +12,7 @@ int main()
     while(row_start<=row_end && col_start<=col_end)
     {
         for(int i=col_start;i<=col_end;i++)
-        arr[row_start][i]=num++;
+        arr[row_start][i]=++num;
         row_start++;
         for(int i=row_start;i<=row_end;i++)
         arr[i][col_end]=num++;
