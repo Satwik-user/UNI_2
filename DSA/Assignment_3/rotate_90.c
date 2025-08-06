221601155ae31f1cc9ce242b80093d3e99c798ee
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:24:40 +0000

    Aug 6, 2025, 4:24 PM

--- DSA/Assignment_3/rotate_90.c
+++ DSA/Assignment_3/rotate_90.c
@@ -13,12 +13,12 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
-    printf("original matrix:\n");
+    printf("Original matrix:\n");
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
         {
-            
+
         }
     }
 
