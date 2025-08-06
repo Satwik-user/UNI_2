4446d435a6fc79578877560d7e6f969105877d22
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:28:58 +0000

    Aug 6, 2025, 4:28 PM

--- DSA/Assignment_3/sparse.c
+++ DSA/Assignment_3/sparse.c
@@ -6,6 +6,12 @@ int main()
     scanf("%d %d",&m,&n);
     int arr[m][n];
     for(int  i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
