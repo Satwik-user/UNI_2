a0fefa489a2600e0c247f04dcfacca5aebd73225
Satwik-user <satwik2400@gmail.com>
2025-08-06 15:30:50 +0000

    Aug 6, 2025, 3:30 PM

--- DSA/Assignment_3/spiral.c
+++ DSA/Assignment_3/spiral.c
@@ -34,7 +34,11 @@ int main()
     printf("Spiral matrix:");
     for(int i=0;i<m;i++)
     {
-        
+        for(int j=0;j<n;j++)
+        {
+            printf("%d",arr[i][j]);
+        }
+        printf("\n");
     }
     
     return 0;
