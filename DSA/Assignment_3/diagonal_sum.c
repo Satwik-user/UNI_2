f9f45f601e48d818c35dda7982d75873d1abf4ed
Satwik-user <satwik2400@gmail.com>
2025-08-06 14:30:42 +0000

    Aug 6, 2025, 2:30 PM

--- DSA/Assignment_3/diagonal_sum.c
+++ DSA/Assignment_3/diagonal_sum.c
@@ -4,7 +4,13 @@ int main()
     int m,n;
     printf("Enter the row and column size:");
     scnaf("%d %d",&m,&n);
-    for(int i=0;i<m)
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
