4420a1f3662ad4ebd82964c5cb5c5200a3a06b59
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:10:32 +0000

    Aug 6, 2025, 4:10 PM

--- DSA/Assignment_3/rotate_90.c
+++ DSA/Assignment_3/rotate_90.c
@@ -7,7 +7,13 @@ int main()
     int arr[m][n];
     for(int i=0;i<m;i++)
     {
-        
+        for(int j=0;j<n;j++)
+        {
+            printf("Enter element:");
+            {
+                
+            }
+        }
     }
     
     return 0;
