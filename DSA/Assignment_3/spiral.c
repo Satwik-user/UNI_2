dd30cda4342a98d83a5d11a2727b97091228271d
Satwik-user <satwik2400@gmail.com>
2025-08-06 14:46:49 +0000

    Aug 6, 2025, 2:46 PM

--- DSA/Assignment_3/spiral.c
+++ DSA/Assignment_3/spiral.c
@@ -5,7 +5,11 @@ int main()
     printf("Enter row and column size:");
     scanf("%d %d",&m,&n);
     for(int i=0;i<m;i++)
-    {}
+    {
+        for(int j=0;j<n;j++)
+        {}
+        
+    }
     
     return 0;
 }
