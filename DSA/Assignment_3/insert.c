c66579a9e2d902663b81ef16ce7f22f1a9f0c817
Satwik-user <satwik2400@gmail.com>
2025-08-06 16:37:25 +0000

    Aug 6, 2025, 4:37 PM

--- DSA/Assignment_3/insert.c
+++ DSA/Assignment_3/insert.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    
+    int m,n;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
