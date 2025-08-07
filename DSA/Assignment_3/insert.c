f8e531f7cd012be04ea55dd257c900667086353b
Satwik-user <satwik2400@gmail.com>
2025-08-07 08:49:09 +0000

    Aug 7, 2025, 8:49 AM

--- DSA/Assignment_3/insert.c
+++ DSA/Assignment_3/insert.c
@@ -49,7 +49,7 @@ int main()
     printf("Modified array:\n");
     for(int i=0;i<=m;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<=n;j++)
         {
             printf("%d ",res[i][j]);
         }
