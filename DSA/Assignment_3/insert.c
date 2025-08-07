58b012bec4898cee8b1919be9eb6326f70729e4a
Satwik-user <satwik2400@gmail.com>
2025-08-07 03:30:30 +0000

    Aug 7, 2025, 3:30 AM

--- DSA/Assignment_3/insert.c
+++ DSA/Assignment_3/insert.c
@@ -4,10 +4,6 @@ int main()
     int m,n,ele,pos_row,pos_col;
     printf("Enter row and column size:");
     scanf("%d %d",&m,&n);
-    printf("Enter element to be inserted:");
-    scanf("%d",&ele);
-    printf("Enter row and column index of new element:");
-    scanf("%d %d",&pos_row,&pos_col);
     int arr[m][n],res[m+1][n+1];
     for(int i=0;i<m;i++)
     {
