3a1eb157c019ad2e22a243bf8ef3ff58dd162ecd
Satwik-user <satwik2400@gmail.com>
2025-08-07 09:24:17 +0000

    Aug 7, 2025, 9:24 AM

--- DSA/Assignment_3/row_change.c
+++ DSA/Assignment_3/row_change.c
@@ -13,6 +13,15 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
+
+    printf("Original matrix:\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+    }
     
     return 0;
 }
