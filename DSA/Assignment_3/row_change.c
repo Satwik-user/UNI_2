59d66d37697031e40053534303c4e4550895fdd1
Satwik-user <satwik2400@gmail.com>
2025-08-07 09:22:41 +0000

    Aug 7, 2025, 9:22 AM

--- DSA/Assignment_3/row_change.c
+++ DSA/Assignment_3/row_change.c
@@ -4,7 +4,13 @@ int main()
     int m,n;
     printf("Enter row and column size:");
     scanf("%d %d",&m,&n);
-    for(int i=0;i<)
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
