1af4b228740426bb4901572e2f459744fed6dd57
Satwik-user <satwik2400@gmail.com>
2025-08-07 03:12:24 +0000

    Aug 7, 2025, 3:12 AM

--- DSA/Assignment_3/insert.c
+++ DSA/Assignment_3/insert.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int m,n,ele,pos;
+    int m,n,ele,pos_row,pos_col;
     printf("Enter row and column size:");
     scnaf("%d %d",&m,&n);
     int arr[m][n];
@@ -13,7 +13,21 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
+    printf("Enter element to be inserted:");
+    scanf("%d",&ele);
+    printf("Enter row and column index of new element:");
+    scanf("%d %d",&pos_row,&pos_col);
+    arr[pos_row][pos_col]=ele;
     
+    printf("Modified array:\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
