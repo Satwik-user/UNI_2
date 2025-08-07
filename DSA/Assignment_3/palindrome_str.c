6cc1c3af3c1b0ab5866fccfc796d5fca3ba2a45c
Satwik-user <satwik2400@gmail.com>
2025-08-07 10:19:47 +0000

    Aug 7, 2025, 10:19 AM

--- DSA/Assignment_3/palindrome_str.c
+++ DSA/Assignment_3/palindrome_str.c
@@ -5,7 +5,7 @@ int main()
     char str[50],rev[50];
     printf("Enter a string:");
     fgets(str,sizeof(str),stdin);
-        str[strlen(str) - 1] = '\0';
+    str[strlen(str) - 1] = '\0';
     strcpy(rev,str);
     int l=0,h=strlen(str)-1;
     while(l<h)
