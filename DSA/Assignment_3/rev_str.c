76e1d31f5678e345ac39d502f472b2fc02eda36c
Satwik-user <satwik2400@gmail.com>
2025-08-07 10:00:47 +0000

    Aug 7, 2025, 10:00 AM

--- DSA/Assignment_3/rev_str.c
+++ DSA/Assignment_3/rev_str.c
@@ -5,8 +5,7 @@ int main()
     char str[50];
     printf("Enetr a string:");
     fgets(str,sizeof(str),stdin);
-    int len=strlen(str);
-    int 
+    int l=0, h=strlen(str;)
     
     return 0;
 }
