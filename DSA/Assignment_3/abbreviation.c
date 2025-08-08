7be41e6adc5026ff14266429a8523dc93981dbca
Satwik-user <satwik2400@gmail.com>
2025-08-08 10:22:55 +0000

    Aug 8, 2025, 10:22 AM

--- DSA/Assignment_3/abbreviation.c
+++ DSA/Assignment_3/abbreviation.c
@@ -2,11 +2,11 @@
 #include <string.h>
 int main()
 {
-    char str[100];
+    char str[100],temp[101];
     printf("Enter a sentence:");
     fgets(str,sizeof(str),stdin);
     str[strlen(str)-1]='\0';
-    
+    temp[0]=' '
     
     return 0;
 }
