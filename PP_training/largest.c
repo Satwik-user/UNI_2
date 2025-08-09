aa0e86f824ab1555a23217a420c551206bb97ec4
Satwik-user <satwik2400@gmail.com>
2025-08-09 12:26:32 +0000

    Aug 9, 2025, 12:26 PM

--- PP_training/largest.c
+++ PP_training/largest.c
@@ -3,6 +3,13 @@ int main()
 {
     int n;
     printf("Enter size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("ENter element:");
+        scanf("%d",&arr[i]);
+    }
     
     
     return 0;
