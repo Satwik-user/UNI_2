0c3e7522843d1f8ca52c3799d615676e542530d7
Satwik-user <satwik2400@gmail.com>
2025-08-09 13:22:31 +0000

    Aug 9, 2025, 1:22 PM

--- PP_training/frequency.c
+++ PP_training/frequency.c
@@ -4,14 +4,14 @@ int main()
     int n;
     printf("Enter size of array:");
     scanf("%d",&n);
-    int arr[n];
+    int arr[n],freq[n];
     for(int i=0;i<n;i++)
     {
         printf("Enter element:");
         scanf("%d",&arr[i]);
     }
 
-    
+
     
     return 0;
 }
