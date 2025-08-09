6faa32ff4d45b0e78352b81341237cf758189ce6
Satwik-user <satwik2400@gmail.com>
2025-08-09 13:15:56 +0000

    Aug 9, 2025, 1:15 PM

--- PP_training/check_sort.c
+++ PP_training/check_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n,f=0;
     printf("Enter size of array:");
     scanf("%d",&n);
     int arr[n];
@@ -11,9 +11,13 @@ int main()
         scanf("%d",&arr[i]);
     }
 
-    for(int i=0;i<n;i++)
+    for(int i=1;i<n;i++)
     {
-        
+        if(arr[i]<arr[i-1])
+        {
+            f=1;
+            break;
+        }
     }
     
     return 0;
