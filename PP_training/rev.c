67a2654465227927c25052b4a211e2cbe3d4e6b9
Satwik-user <satwik2400@gmail.com>
2025-08-09 14:05:20 +0000

    Aug 9, 2025, 2:05 PM

--- PP_training/rev.c
+++ PP_training/rev.c
@@ -4,13 +4,17 @@ int main()
     int n;
     printf("Enter size of array:");
     scanf("%d",&n);
-    int arr[n];
+    int arr[n],rev[n];
     for(int i=0;i<n;i++)
     {
         printf("Enter element:");
         scanf("%d",&arr[i]);
     }
-    
+
+    for(int i=0;i<n;i++)
+    {
+        rev[i]=arr[n-i-1];
+    }
     
     return 0;
 }
