72e7b9a2a7991e80a4b4c3bd01a548e3b26b9d2b
Satwik-user <satwik2400@gmail.com>
2025-08-10 13:08:09 +0000

    Aug 10, 2025, 1:08 PM

--- DSA/Assignment_3/insert.py
+++ DSA/Assignment_3/insert.py
@@ -1,7 +1,7 @@
 m=int(input("Enter row size:"))
 n=int(input("Enter column size:"))
 arr=[]
-for i in range(m):
+for i in range(m+1):
     a=[]
     for j in range(n):
         a.append(int(input("Enter element:")))
