3958b60860ae864a04a6025071fd5ee5babf460b
Satwik-user <satwik2400@gmail.com>
2025-08-10 18:34:40 +0000

    Aug 10, 2025, 6:34 PM

--- DSA/Assignment_3/insert.py
+++ DSA/Assignment_3/insert.py
@@ -16,4 +16,6 @@ pos_row=int(input("Enter row position:"))
 pos_col=int(input("Enter column position:"))
 
 if pos_row<0 or pos_row>m or pos_col<0 or pos_col>n:
-    print("Invalid position for insertion")
+    print("Invalid position for insertion!")
+else:
+    mod
