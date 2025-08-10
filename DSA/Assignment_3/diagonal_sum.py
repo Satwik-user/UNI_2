cea576fc6a14d1b31a5161f329f56e1ad5d4c22b
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:10:53 +0000

    Aug 10, 2025, 12:10 PM

--- DSA/Assignment_3/diagonal_sum.py
+++ DSA/Assignment_3/diagonal_sum.py
@@ -5,7 +5,7 @@ sum=0
 for i in range(m):
     a=[]
     for j in range(n):
-        aint(input("Enter element:"))
+        a.append(int(input("Enter element:")))
 
 for i in range(m):
     for j in range(n):
