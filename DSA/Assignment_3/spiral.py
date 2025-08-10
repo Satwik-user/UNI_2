78a1ca52a0814e526df3fc270872a564ecbb62c5
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:30:24 +0000

    Aug 10, 2025, 12:30 PM

--- DSA/Assignment_3/spiral.py
+++ DSA/Assignment_3/spiral.py
@@ -3,17 +3,20 @@ n=int(input("Enter column size:"))
 arr=[]
 arr=[[0 for _ in range(m)] for _ in range(n)]
 
-row_start, row_end=0, m-1
-col_start, col_end=0, n-1
+row_start, row_end=0, m
+col_start, col_end=0, n
 num=0
 
 while(row_start<=row_end and col_start<=col_end):
-    for i in range(col_start,col_end-1):
+    for i in range(col_start,col_end):
         num+=1
         arr[row_start][i]=num
     row_start+=1
 
-    for i in range(row_start,row_end+1):
+    for i in range(row_start,row_end):
         num+=1
         arr[i][col_end]=num
-    
+    col_end-=1
+
+    for i in range(col_end-1,col_start,-1):
+        
