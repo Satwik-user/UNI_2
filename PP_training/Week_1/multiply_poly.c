4366ef22a33edbf2b02d17ecb2a3a126d3f6a330
Satwik-user <satwik2400@gmail.com>
2025-08-10 08:13:55 +0000

    Aug 10, 2025, 8:13 AM

--- PP_training/Week_1/multiply_poly.c
+++ PP_training/Week_1/multiply_poly.c
@@ -5,7 +5,10 @@ int main()
     printf("Enter size of two polynomials:");
     scanf("%d %d",&m,&n);
     int A[m],B[n];
-    
+    for(int i=0;i<m;i++)
+    {
+        printf("Enter element ")
+    }
     
     return 0;
 }
