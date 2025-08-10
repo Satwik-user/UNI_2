9935bc262bc2f4d453c0a36cac06cf33abd1a18f
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:46:44 +0000

    Aug 10, 2025, 12:46 PM

--- DSA/Assignment_3/rotate_90.py
+++ DSA/Assignment_3/rotate_90.py
@@ -1,9 +1,14 @@
 m=int(input("Enter row size:"))
 n=int(input("Enter column size:"))
 arr=[]
+rot=[]
 for i in range(m):
     a=[]
     for j in range(n):
         a.append(int(input("Enter element:")))
     arr.append(a)
 
+rot=[[0 for _ in range(n)]for _ in range(m)]
+for i in range(m):
+    for j in range(n):
+        rot[]
