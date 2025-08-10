26b1e10b6f99e5122f74acf9e2df10c5463f5fbc
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:25:51 +0000

    Aug 10, 2025, 12:25 PM

--- DSA/Assignment_3/spiral.py
+++ DSA/Assignment_3/spiral.py
@@ -6,3 +6,8 @@ arr=[[0 for _ in range(m)] for _ in range(n)]
 row_start, row_end=0, m-1
 col_start, col_end=0, n-1
 num=0
+
+while(row_start<=row_end and col_start<=col_end):
+    for i in range(col_start,col_end-1):
+        num+=1
+        
