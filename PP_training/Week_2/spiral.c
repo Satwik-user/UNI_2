0c62a0f0bd0e6590d895feec3af3bd4bb78bc262
Satwik-user <satwik2400@gmail.com>
2025-08-10 04:39:03 +0000

    Aug 10, 2025, 4:39 AM

--- PP_training/Week_2/spiral.c
+++ PP_training/Week_2/spiral.c
@@ -27,6 +27,7 @@ int main()
     
     int row_start=0,row_end=m-1;
     int col_start=0,col_end=n-1;
+    printf("Spiral Array:");
     while(row_start<=row_end && col_start<=col_end)
     {
         for(int i=col_start;i<=col_end;i++)
