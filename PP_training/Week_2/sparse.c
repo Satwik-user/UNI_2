0aebba8e87ec9546793e2fd2512caeb092e7e754
Satwik-user <satwik2400@gmail.com>
2025-08-10 04:57:22 +0000

    Aug 10, 2025, 4:57 AM

--- PP_training/Week_2/sparse.c
+++ PP_training/Week_2/sparse.c
@@ -15,7 +15,13 @@ int main()
     }
 
     printf("Given Matrix:\n");
-    
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            
+        }
+    }
 
     int c=0;
     for(int i=0;i<m;i++)
