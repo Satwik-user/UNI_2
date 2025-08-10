b35302df478c242e555a5fe87d6489f38552794a
Satwik-user <satwik2400@gmail.com>
2025-08-10 11:55:52 +0000

    Aug 10, 2025, 11:55 AM

--- PP_training/Week_2/count_zeros.c
+++ PP_training/Week_2/count_zeros.c
@@ -2,7 +2,17 @@
 int main()
 {
     int m,n;
-    
+    printf("Enter row and column sizes:");
+    scanf("%d %d",&m,&n);
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("Enetr element:");
+            scanf("%d",&arr[i][j]);
+        }
+    }
     
     return 0;
 }
