ba91d6c983ce5ae731224ce3d8264218e94c03ed
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:24:30 +0000

    Aug 10, 2025, 12:24 PM

--- DSA/Assignment_3/spiral.py
+++ DSA/Assignment_3/spiral.py
@@ -3,5 +3,5 @@ n=int(input("Enter column size:"))
 arr=[]
 arr=[[0 for _ in range(m)] for _ in range(n)]
 
-row_start, row_end=0,m-1
-col_start, col_end=0,n-1
+row_start, row_end=0, m-1
+col_start, col_end=0, n-1
