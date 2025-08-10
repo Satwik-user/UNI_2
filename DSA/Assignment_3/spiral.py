bd4d9b5200b70ce3da3fbf55788417c3df2d9daf
Satwik-user <satwik2400@gmail.com>
2025-08-10 12:13:57 +0000

    Aug 10, 2025, 12:13 PM

--- DSA/Assignment_3/spiral.py
+++ DSA/Assignment_3/spiral.py
@@ -2,6 +2,7 @@ m=int(input("Enter row size:"))
 n=int(input("Enter column size:"))
 arr=[]
 for i in range(m):
+    a=[]
     for j in range(n):
         a.append(int(input("Enter element:")))
     arr.append(a)
