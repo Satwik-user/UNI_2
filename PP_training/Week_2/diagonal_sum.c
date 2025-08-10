968e7407ae6e24fd12e487f854bab93bde57a475
Satwik-user <satwik2400@gmail.com>
2025-08-10 04:43:21 +0000

    Aug 10, 2025, 4:43 AM

---
+++ PP_training/Week_2/diagonal_sum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int m,n,s=0;
+    printf("Enter the row and column size:");
+    scanf("%d %d",&m,&n);
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("Enter element:");
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    if(m==n)
+    {
+        for(int i=0;i<m;i++)
+        {
+            s+=arr[i][i];
+        }
+        printf("Sum of diagonal elements:%d\n",s);
+    }
+    else
+    printf("Not a square matrix\nSum not possible");
+    
+    return 0;
+}
