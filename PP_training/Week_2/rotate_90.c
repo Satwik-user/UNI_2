b803790c40f21f77208beebbb57e7e0f2d3e86d9
Satwik-user <satwik2400@gmail.com>
2025-08-10 04:39:50 +0000

    Aug 10, 2025, 4:39 AM

---
+++ PP_training/Week_2/rotate_90.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+    int m,n;
+    printf("Enter row and column size:");
+    scanf("%d %d",&m,&n);
+    int arr[m][n],rot[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("Enter element:");
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("Original matrix:\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            rot[j][n-i-1]=arr[i][j];
+        }
+    }
+
+    printf("Rotated matrix:\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",rot[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
