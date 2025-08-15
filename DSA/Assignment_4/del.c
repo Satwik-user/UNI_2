744461f8bc0dc3b291d40e51beee6ab1ec4d8f7d
Satwik-user <satwik2400@gmail.com>
2025-08-15 14:53:21 +0000

    Aug 15, 2025, 2:53 PM

--- DSA/Assignment_4/del.c
+++ DSA/Assignment_4/del.c
@@ -10,7 +10,12 @@ int main()
     int n;
     printf("Enter number of nodes:");
     scanf("%d",&n);
-    
+    Node *head=NULL;
+    Node *tail=NULL;
+    for(int i=0;i<n;i++)
+    {
+        
+    }
     
     return 0;
 }
