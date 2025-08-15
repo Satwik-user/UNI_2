ec6ca9a6500db367893e82c427af9281ad6e4642
Satwik-user <satwik2400@gmail.com>
2025-08-15 11:53:47 +0000

    Aug 15, 2025, 11:53 AM

---
+++ DSA/Assignment_4/insertion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+
+int main()
+{
+    int n;
+    printf("Enter number of nodes:");
+    scanf("%d",&n);
+    Node *head=NULL;
+    Node *tail=NULL;
+    for(int i=0;i<n;i++)
+    {
+        Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter element:");
+        scanf("%d",&cur->data);
+        cur->next=NULL;
+
+        if(head==NULL)
+        {
+            head=cur;
+            tail=cur;
+        }
+        else
+        {
+            tail->next=cur;
+            tail=cur;
+        }
+    }
+    
+    return 0;
+}
