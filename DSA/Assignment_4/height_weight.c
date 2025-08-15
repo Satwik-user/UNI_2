bb39de953501c584aa8dc01d6675d087426fd791
Satwik-user <satwik2400@gmail.com>
2025-08-15 08:58:03 +0000

    Aug 15, 2025, 8:58 AM

--- DSA/Assignment_4/height_weight.c
+++ DSA/Assignment_4/height_weight.c
@@ -17,7 +17,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         Node *cur=(Node *)malloc(sizeof(Node));
-        printf("Enter height and weight of student%d:",i+1);
+        printf("Enter height and weight of student %d:",i+1);
         scanf("%d %d",&cur->height,&cur->weight);
         cur->next=NULL;
 
