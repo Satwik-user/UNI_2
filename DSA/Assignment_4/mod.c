ff32b32fae783b7dd48f9db1e109390c6701b041
Satwik-user <satwik2400@gmail.com>
2025-08-15 14:33:43 +0000

    Aug 15, 2025, 2:33 PM

--- DSA/Assignment_4/mod.c
+++ DSA/Assignment_4/mod.c
@@ -16,6 +16,8 @@ int main()
     for(int i=0;i<n;i++)
     {
         Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter element:");
+        scanf("%d",&cur->data);
         
     }
     
