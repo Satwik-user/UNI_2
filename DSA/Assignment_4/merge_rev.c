870f1101a4fe5e279c3c05feee19d8b67338a6ec
Satwik-user <satwik2400@gmail.com>
2025-08-16 05:33:57 +0000

    Aug 16, 2025, 5:33 AM

--- DSA/Assignment_4/merge_rev.c
+++ DSA/Assignment_4/merge_rev.c
@@ -51,7 +51,7 @@ int main()
         }
     }
     
-    tail->next=head_1;//Connecting last node of 1st list with 2nd
+    tail->next=head_1;//Connecting tail node of 1st list with 2nd
 
     printf("Merged List:");
     Node *temp=head;
