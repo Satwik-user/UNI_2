44c2541423fc1e6586c9e671188ecf066cbb9b7c
Satwik-user <satwik2400@gmail.com>
2025-08-16 06:16:07 +0000

    Aug 16, 2025, 6:16 AM

--- DSA/Assignment_4/left_shift.c
+++ DSA/Assignment_4/left_shift.c
@@ -10,10 +10,11 @@ int main()
     int n;
     printf("Enter number of nodes:");
     scanf("%d",&n);
-    
+    Node *head=NULL;
+    Node *tail=NULL;
     for(int i=0;i<n;i++)
     {
-
+        
     }
     
     return 0;
