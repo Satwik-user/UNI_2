90cc62840fa5c9614c732f04295a3b8b2f290539
Satwik-user <satwik2400@gmail.com>
2025-08-28 10:05:10 +0000

    Aug 28, 2025, 10:05 AM

---
+++ left_shift.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+int main()
+{
+    int n;
+    printf("Enter number of nodes:");
+    scanf("%d",&n);
+    Node *head=NULL;
+    Node *tail=NULL;
+    for(int i=0;i<n;i++)
+    {
+        Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter element:");
+        scanf("%d",&cur->data);
+        cur->next=NULL;
+        if(head==NULL)
+        {
+            head=cur;
+            tail=cur;
+        }
+        else
+        {
+            tail->next=cur;
+            tail=cur;
+        }
+    }
+
+    printf("Original List:");
+    Node *temp=head;
+    while(temp)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+
+    int k;
+    printf("Enter number of nodes to rotate:");
+    scanf("%d",&k);
+    if(k<0 || k>n)
+    printf("No of nodes should be positive and less than size of list!");
+    else
+    {
+        for(int i=0;i<k;i++)
+        {
+            Node *old_head=head;
+            head=head->next;
+            tail->next=old_head;
+            tail=old_head;
+            tail->next=NULL;
+        }
+        printf("List after %d rotations:",k);
+        temp=head;
+        while(temp)
+        {
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("\n");
+    }
+
+    //Free memory
+    temp=head;
+    while(temp)
+    {
+        Node *next_node=temp->next;
+        free(temp);
+        temp=next_node;
+    }
+    
+    return 0;
+}
