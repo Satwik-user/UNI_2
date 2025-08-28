c5651dadd8830072ff73786f87c978994f6e022a
Satwik-user <satwik2400@gmail.com>
2025-08-28 10:03:10 +0000

    Aug 28, 2025, 10:03 AM

---
+++ merge.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+int main()
+{
+    int n1,n2;
+    printf("Enter number of nodes for 1st and 2nd list:");
+    scanf("%d %d",&n1,&n2);
+    Node *head=NULL,*head_1=NULL;
+    Node *tail=NULL,*tail_1=NULL;
+    printf("Enter sorted lists-\n");
+    for(int i=0;i<n1;i++)
+    {
+        Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter element for 1st list:");
+        scanf("%d",&cur->data);
+        cur->next=NULL;
+
+        if(head==NULL)
+        {
+            head=cur;
+            tail=cur;
+        }
+        else
+        {
+            tail->next=cur;
+            tail=cur;
+        }
+    }
+    printf("\n");
+    for(int i=0;i<n2;i++)
+    {
+        Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter element for 2nd list:");
+        scanf("%d",&cur->data);
+        cur->next=NULL;
+
+        if(head_1==NULL)
+        {
+            head_1=cur;
+            tail_1=cur;
+        }
+        else
+        {
+            tail_1->next=cur;
+            tail_1=cur;
+        }
+    }
+    
+    tail->next=head_1;//Connecting tail node of 1st list with head node of 2nd
+
+    printf("Merged List:");
+    Node *temp=head;
+    while(temp)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+
+    //Free memory
+    temp=head;
+    while(temp)
+    {
+        Node *next_node=temp->next;
+        free(temp);
+        temp=next_node;
+    }
+    
+    return 0;
+}
