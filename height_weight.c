d05a969f43212bf29686b534e06a438a24e4e39a
Satwik-user <satwik2400@gmail.com>
2025-08-28 09:01:19 +0000

    Aug 28, 2025, 9:01 AM

---
+++ height_weight.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int height;
+    int weight;
+    struct Node *next;
+}Node;
+
+int main()
+{
+    int n;
+    printf("Enter number of students:");
+    scanf("%d",&n);
+    Node *head=NULL;
+    Node *tail=NULL;
+    for(int i=0;i<n;i++)
+    {
+        Node *cur=(Node *)malloc(sizeof(Node));
+        printf("Enter height and weight of student %d:",i+1);
+        scanf("%d %d",&cur->height,&cur->weight);
+        cur->next=NULL;
+
+        if(head==NULL)
+        {
+            head=cur;
+            tail=cur;
+        }
+        else
+        {
+            tail->next=cur;
+            tail=cur;
+        }
+    }
+
+    Node *temp=head;
+    int max=0;
+    while(temp)
+    {
+        int ratio=temp->weight/temp->height;
+        if(ratio>max)
+        max=ratio;
+        temp=temp->next;
+    }
+
+    printf("Highest weight/height ratio:%d\n",max);
+
+    //Free memory
+    temp=head;
+    while(temp)
+    {
+        Node *next_node=temp->next;
+        free(temp);
+        temp=next_node;
+    }
+    
+    return 0;
+}
---
+++ implement_ll.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+
+int main()
+{
+    int n;
+    printf("Enter number of nodes:");
+    scanf("%d",&n);
+    Node *head=NULL;
+    Node *tail=NULL;
+    for(int i=0;i<n;i++)
+    {
+        Node *current=(Node *)malloc(sizeof(Node));
+        printf("Enter data of node %d:",i+1);
+        scanf("%d",&current->data);
+        current->next=NULL;
+
+        if(head==NULL)
+        {
+            head=current;
+            tail=current;
+        }
+        else
+        {
+            tail->next=current;
+            tail=current;
+        }
+    }
+
+    printf("Linked List elements:");
+    Node *temp=head;
+    while(temp)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+
+    //Free memory
+    temp=head;
+    while(temp)
+    {
+        Node *next_node=temp->next;
+        free(temp);
+        temp=next_node;
+    }
+    
+    return 0;
+}
