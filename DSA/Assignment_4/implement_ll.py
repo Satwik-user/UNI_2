f0a5f3659174a848412160f2846873db3e45d0ca
Satwik-user <satwik2400@gmail.com>
2025-09-03 16:02:42 +0000

    Sep 3, 2025, 4:02 PM

--- DSA/Assignment_4/implement_ll.py
+++ DSA/Assignment_4/implement_ll.py
@@ -3,6 +3,10 @@ class Node:
         self.data=data
         self.next=None
 
+class Linked_list:
+    def __init__(self):
+        self.head=None
+        
     def insert_ele(self, data):
         new_Node=Node(data)
         if self.head is None:
@@ -12,3 +16,5 @@ class Node:
         while cur.next:
             cur=cur.next
         cur.next=new_Node
+
+if __name__=="__main__":
