bd5f4ad74890b989edc4d315f20ea3878b142be1
Satwik-user <satwik2400@gmail.com>
2025-09-04 09:05:13 +0000

    Sep 4, 2025, 9:05 AM

--- DSA/Assignment_4/height_weight.py
+++ DSA/Assignment_4/height_weight.py
@@ -1,4 +1,10 @@
 class Node:
     def __init__(self,data):
         self.data=data
-        self.next
+        self.next=None
+
+class Linked_list:
+    def __init__(self):
+        self.head=None
+
+    
