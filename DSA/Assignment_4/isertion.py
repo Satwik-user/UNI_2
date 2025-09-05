b6dd4275ab91061a4c6cbb4a1948dcc9d0ac6047
Satwik-user <satwik2400@gmail.com>
2025-09-05 15:30:41 +0000

    Sep 5, 2025, 3:30 PM

--- DSA/Assignment_4/isertion.py
+++ DSA/Assignment_4/isertion.py
@@ -1,5 +1,8 @@
 class Node:
-    def __init__(self, head):
-        self.head=head
+    def __init__(self, data):
+        self.data=data
         self.next=None
+
+class Linked_list:
+    def __init__(self, head):
         
