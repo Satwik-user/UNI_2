31a748f52df3d5901520fdcae22d5d2dae57a6a0
Satwik-user <satwik2400@gmail.com>
2025-09-05 16:58:19 +0000

    Sep 5, 2025, 4:58 PM

--- DSA/Assignment_4/mod.py
+++ DSA/Assignment_4/mod.py
@@ -10,4 +10,6 @@ class Linked_list:
     def insert_ele(self, data):
         new_node=Node(data)
         if self.head is None:
-            self.head=
+            self.head=new_node
+            return
+        
