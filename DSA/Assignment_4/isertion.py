d64f4990ae7d565220f18fcc6521c3502eba0bd1
Satwik-user <satwik2400@gmail.com>
2025-09-05 16:18:24 +0000

    Sep 5, 2025, 4:18 PM

--- DSA/Assignment_4/isertion.py
+++ DSA/Assignment_4/isertion.py
@@ -31,7 +31,7 @@ for i in range(n):
     data=int(input("Enter an element:"))
     list.insert_ele(data)
 
-print("")
+print("Original elements of list:")
 
 ele=int(input("Enter element to insert:"))
 pos=int(input("Enter position of element:"))
