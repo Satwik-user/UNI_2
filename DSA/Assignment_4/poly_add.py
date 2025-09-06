beef9b6637d42cdfa1c9a988371b4074d463194c
Satwik-user <satwik2400@gmail.com>
2025-09-06 10:23:46 +0000

    Sep 6, 2025, 10:23 AM

--- DSA/Assignment_4/poly_add.py
+++ DSA/Assignment_4/poly_add.py
@@ -33,7 +33,7 @@ for i in range(n1):
     list1.insert_ele(coeff1, exp1)
 n2=int(input("Enter number of nodes for 2nd polynomial:"))
 for i in range(n2):
-    coeff
+    coeff2,exp2
 
 print("Original List:",end=' ')
 list1.print_list()
