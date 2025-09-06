314bb0be840d047ab69a238b6a492d5514998422
Satwik-user <satwik2400@gmail.com>
2025-09-06 05:19:31 +0000

    Sep 6, 2025, 5:19 AM

--- DSA/Assignment_4/merge_rev.py
+++ DSA/Assignment_4/merge_rev.py
@@ -37,7 +37,8 @@ for i in range(n2):
     ele2=int(input("Enter element:"))
     list2.insert_ele(ele2)
 
-print("Original ")
+print("Original List:",end=' ')
+list
 
 temp=list1.head
 while temp.next:
