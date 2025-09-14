c3fa9565f1fec0d251029beefdd434bc539c28d5
Satwik-user <satwik2400@gmail.com>
2025-09-14 05:33:33 +0000

    Sep 14, 2025, 5:33 AM

--- DSA/Assignment_5/doubly.c
+++ DSA/Assignment_5/doubly.c
@@ -6,11 +6,11 @@ typedef struct Node
     struct Node *prev;
     struct Node *next;
 }Node;
-void insert_ele(int data)
+Node *createNode(int data)
 {
     Node *new_node=(Node *)malloc(sizeof(Node));
     new_node->data=data;
     new_node->prev=NULL;
     new_node->next=NULL;
-    
+
 }
