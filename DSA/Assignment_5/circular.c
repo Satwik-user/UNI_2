032a5fba1326d664d114924607783c927133e9da
Satwik-user <satwik2400@gmail.com>
2025-10-04 07:18:23 +0000

    Oct 4, 2025, 7:18 AM

--- DSA/Assignment_5/circular.c
+++ DSA/Assignment_5/circular.c
@@ -11,6 +11,7 @@ Node *createNode(int data)
     Node *newNode=(Node *)malloc(sizeof(Node));
     newNode->data=data;
     newNode->next=NULL;
+    return newNode;
 }
 
 void insert_ele(Node* *head, int data)
