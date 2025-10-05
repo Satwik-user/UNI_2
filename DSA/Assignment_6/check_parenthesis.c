e46ba5ee66b2c6d1ab988efb92c3dd1dff13881f
Satwik-user <satwik2400@gmail.com>
2025-10-05 14:03:04 +0000

    Oct 5, 2025, 2:03 PM

--- DSA/Assignment_6/check_parenthesis.c
+++ DSA/Assignment_6/check_parenthesis.c
@@ -11,5 +11,6 @@ stack *createStack(int cap)
 {
     stack *newStack=(stack *)malloc(sizeof(stack));
     newStack->cap=cap;
-
+    newStack->top=-1;
+    newStack->arr=(int *)malloc(cap*sizeof(int));
 }
