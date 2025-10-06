3103c49838d4eeddf96690eef9e748a9da644a2b
Satwik-user <satwik2400@gmail.com>
2025-10-06 12:59:00 +0000

    Oct 6, 2025, 12:59 PM

--- DSA/Assignment_6/in-post.c
+++ DSA/Assignment_6/in-post.c
@@ -1,74 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-int prec(char c) {
-    if (c == '^')
-        return 3;
-    else if (c == '/' || c == '*')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
+int prec(char c)
+{
+    if(c=='^')
+    return 3;
+    else if(c=='*' || c=='/')
+    return 2;
+    else if(c=='+' || c=='-')
+    return 1;
     else
-        return -1;
+    return -1;
 }
 
-// Check if operator is right-associative
-int isRightAssociative(char c) {
-    return c == '^';
+int isRightAssociative(char c)
+{
+    return c=='^';
 }
 
-void infixToPostfix(char* exp) {
-    int len = strlen(exp);
-    char result[len + 1];
+char *infixToPostfix(char *exp)
+{
+    int len=strlen(exp);
+    char res[len+1];
     char stack[len];
-    int j = 0;
-    int top = -1;
+    int j=0;
+    int top=-1;
 
-    for (int i = 0; i < len; i++) {
-        char c = exp[i];
+    for(int i=0;i<len;i++)
+    {
+        char c=exp[i];
 
-        // If operand, add to result
-        if (isalnum(c)) {
-            result[j++] = c;
+        //operand
+        if(isalnum(c))
+        res[j++]=c;
+
+        //opening parenthesis
+        else if(c=='(')
+        stack[++top]=c;
+
+        //closing parentheses
+        else if(c==')')
+        {
+            while(top!=-1 && c!='(')
+            res[j++]=stack[top--];
+            top--;
         }
-        // If '(', push to stack
-        else if (c == '(') {
-            stack[++top] = '(';
-        }
-        // If ')', pop until '('
-        else if (c == ')') {
-            while (top != -1 && stack[top] != '(') {
-                result[j++] = stack[top--];
-            }
-            
-            // pop '('
-            top--; 
-        }
-        
-        // If operator
-        else {
-            while (top != -1 && stack[top] != '(' &&
-                  (prec(stack[top]) > prec(c) ||
-                  (prec(stack[top]) == prec(c) && !isRightAssociative(c)))) {
-                result[j++] = stack[top--];
-            }
-            stack[++top] = c;
+
+        //operator
+        else
+        {
+            while(top!=-1 && stack[top]!='(' && (prec(stack[top])>prec(c) || (prec(stack[top])==prec(c) && !isRightAssociative(c))))
+            res[j++]=stack[--top];
+            stack[++top]=c;
         }
     }
-
-    // Pop remaining operators
-    while (top != -1) {
-        result[j++] = stack[top--];
-    }
-
-    result[j] = '\0';
-    printf("%s\n", result);
+    while(top!=-1)
+    res[j++]=stack[top--];
+    res[j]='\0';
+    return res;
 }
 
-int main() {
-    char exp1[] = "a*(b+c)/d";
-    infixToPostfix(exp1); 
+int main()
+{
+    int n;
+    printf("Enter length of expression:");
+    scanf("%d",&n);
+
+    char exp[n];
+    printf("Enter infix expression:");
+    scanf("%s",&exp);
+
+    printf("Converted to postfix expression:%s",infixToPostfix(exp));
+    
     return 0;
 }
