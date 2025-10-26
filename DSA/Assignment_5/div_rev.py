90eb2a4a654f9b9fb1eb2357f7f4be53d5f0a90f
Satwik-user <satwik2400@gmail.com>
2025-10-26 19:07:40 +0000

    Oct 26, 2025, 7:07 PM

---
+++ DSA/Assignment_5/div_rev.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head=None
+
+    def createNode(self, data):
+        return Node(data)
+    
+    def insert_ele(self, data):
+        newNode=self.createNode(data)
+        if self.head is None:
+            self.head=newNode
