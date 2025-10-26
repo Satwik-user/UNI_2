3c138ec4aa63c843dc9c7985f62aff33c52e9ddc
Satwik-user <satwik2400@gmail.com>
2025-10-26 11:31:36 +0000

    Oct 26, 2025, 11:31 AM

--- DSA/Assignment_5/doubly_circular.py
+++ DSA/Assignment_5/doubly_circular.py
@@ -121,4 +121,4 @@ def main():
     #Insertion of element at end
     ele=int(input("Enter element to insert at end:"))
     dcll.insert_ele(ele)
-    print("List after insertion at ")
+    print("List after insertion at end:",end=)
