babdc134869d0de7ac99f5d5b6f5e9792dc8831d
Satwik-user <satwik2400@gmail.com>
2025-10-26 11:45:05 +0000

    Oct 26, 2025, 11:45 AM

--- DSA/Assignment_5/doubly_circular.py
+++ DSA/Assignment_5/doubly_circular.py
@@ -142,4 +142,4 @@ def main():
     dcll.print_list()
 
     #Deletion of element at specific position
-    pos=int(input("Enter position of "))
+    pos=int(input("Enter position of element to dele"))
