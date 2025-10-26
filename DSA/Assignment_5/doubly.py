b70ced59b707aa6d21fae5f7f1634592b63ec15a
Satwik-user <satwik2400@gmail.com>
2025-10-26 11:08:11 +0000

    Oct 26, 2025, 11:08 AM

--- DSA/Assignment_5/doubly.py
+++ DSA/Assignment_5/doubly.py
@@ -51,7 +51,7 @@ class DoublyLinkedList():
             print("Empty List!")
             return
         self.head=self.head.next
-        self.head
+        self.head.prev=None
 
     def del_end(self):
         if self.head is None:
