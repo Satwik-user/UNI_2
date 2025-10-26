dd4d104e181e5073c5a090f8d47c7bce570e0c5a
Satwik-user <satwik2400@gmail.com>
2025-10-26 07:45:48 +0000

    Oct 26, 2025, 7:45 AM

--- DSA/Assignment_5/doubly_circular.py
+++ DSA/Assignment_5/doubly_circular.py
@@ -49,6 +49,4 @@ class DoublyCircularLinkedList():
         temp=self.head
         for _ in range(1, pos-1):
             temp=temp.next
-        temp.next.prev=temp.prev
         temp.prev.next=temp.next
-        
