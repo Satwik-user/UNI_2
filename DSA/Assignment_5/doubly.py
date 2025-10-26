9667be458e6ce267c2b70db8456a7184732342ab
Satwik-user <satwik2400@gmail.com>
2025-10-26 05:33:01 +0000

    Oct 26, 2025, 5:33 AM

--- DSA/Assignment_5/doubly.py
+++ DSA/Assignment_5/doubly.py
@@ -73,4 +73,5 @@ class DoublylinkedList():
         for _ in range(1, pos-1):
             temp=temp.next
         temp.prev.next=temp.next
-        
+        if temp.next:
+            temp.next.prev=temp.pre
