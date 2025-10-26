dd1befc7329e97b58352520d30d93a3e4fa1ac81
Satwik-user <satwik2400@gmail.com>
2025-10-26 04:35:24 +0000

    Oct 26, 2025, 4:35 AM

--- DSA/Assignment_5/circular.py
+++ DSA/Assignment_5/circular.py
@@ -114,4 +114,4 @@ def main():
     #Insertion of element at start
     ele=int(input("Enter element to insert at start:"))
     cll.insert_start(ele)
-    print("List after insertion at start:")
+    print("List after insertion at start:",end='')
