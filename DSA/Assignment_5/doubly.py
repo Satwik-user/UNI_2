2c1ff3f7da77b9c267aa11bb69236a23b7326487
Satwik-user <satwik2400@gmail.com>
2025-10-26 05:49:56 +0000

    Oct 26, 2025, 5:49 AM

--- DSA/Assignment_5/doubly.py
+++ DSA/Assignment_5/doubly.py
@@ -131,5 +131,5 @@ def main():
     #Deletion of element at specific position
     pos=int(input("Enter position of deletion of element:"))
     dll.del_pos(pos)
-    print("List after deletion at position:")
+    print("List after deletion at position:",end='')
     dll.print_list()
