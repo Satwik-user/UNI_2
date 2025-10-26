bdcf7629c58310f74cd557a02b677016068c3840
Satwik-user <satwik2400@gmail.com>
2025-10-26 06:43:55 +0000

    Oct 26, 2025, 6:43 AM

--- DSA/Assignment_5/doubly_circular.py
+++ DSA/Assignment_5/doubly_circular.py
@@ -3,3 +3,10 @@ class Node:
         self.data=data
         self.next=None
         self.prev=None
+
+class DoublyCircularLinkedList():
+    def __init__(self):
+        self.head=None
+
+    def createNode(self, data):
+        return Node
