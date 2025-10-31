ba37ce4a673e83806de06e28d8477c1a1f4316e4
Satwik-user <satwik2400@gmail.com>
2025-10-31 16:00:00 +0000

    Oct 31, 2025, 4:00 PM

--- DSA/Assignment_6/tower_of_hanoi.py
+++ DSA/Assignment_6/tower_of_hanoi.py
@@ -3,4 +3,4 @@ def towerOfHanoi(n, from_rod, to_rod, aux_rod):
         return
     towerOfHanoi(n-1, from_rod, aux_rod, to_rod)
     print(f"Move disk {n} from rod {from_rod} to rod{to_rod}")
-    towerOfHanoi(n-1, )
+    towerOfHanoi(n-1, au)
