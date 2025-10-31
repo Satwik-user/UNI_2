ce4b6fac69ef20e6d30001fc9e69b3c0f49f84e0
Satwik-user <satwik2400@gmail.com>
2025-10-31 16:07:13 +0000

    Oct 31, 2025, 4:07 PM

--- DSA/Assignment_6/decimal-binary.py
+++ DSA/Assignment_6/decimal-binary.py
@@ -2,4 +2,8 @@ class Stack:
     def __init__(self, cap):
         self.arr=[]
         self.cap=cap
-        self.
+        self.top=-1
+
+    @staticmethod
+    def createStack(capacity):
+        return 
