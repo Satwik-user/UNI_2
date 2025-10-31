e65d35f4c55e60b7192169e637be6c07d4b0710e
Satwik-user <satwik2400@gmail.com>
2025-10-31 03:38:33 +0000

    Oct 31, 2025, 3:38 AM

---
+++ DSA/Assignment_6/in-post.py
@@ -0,0 +1,30 @@
+def prec(c):
+    if c=='^':
+        return 3
+    elif c=='*' or c=='/':
+        return 2
+    elif c=='+' or c=='-':
+        return 1
+    else:
+        return -1
+    
+def isRightAssociative(c):
+    return c=='^'
+
+def infixToPostfix(exp):
+    len=len(exp)
+    res=[]
+    stack=[]
+    for i in range(len):
+        c=exp[i]
+        
+        #operands
+        if c.isalnum():
+            res.append(c)
+
+        #opening parenthesis
+        elif c=='(':
+            stack.append(c)
+
+        elif c==')':
+            while stack and 
