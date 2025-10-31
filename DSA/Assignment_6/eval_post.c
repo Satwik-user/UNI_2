24e9efe4c95539b975674e3de14fa7c7cecf5b90
Satwik-user <satwik2400@gmail.com>
2025-10-31 05:02:50 +0000

    Oct 31, 2025, 5:02 AM

--- DSA/Assignment_6/eval_post.c
+++ DSA/Assignment_6/eval_post.c
@@ -75,7 +75,7 @@ void evaluatePostfix(char **exp, int len)
             push(stack, a-b);
             else if(c=='*')
             push(stack, a*b);
-            else if(strcmp(c,'/')==0)
+            else if(c=='/')
             push(stack, a/b);
             else if(strcmp(c,'^')==0)
             push(stack, (int)pow(a,b));
