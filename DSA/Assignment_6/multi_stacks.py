3d71cadc703a55bb27d41e57f565e7f666e464de
Satwik-user <satwik2400@gmail.com>
2025-10-31 19:28:14 +0000

    Oct 31, 2025, 7:28 PM

--- DSA/Assignment_6/multi_stacks.py
+++ DSA/Assignment_6/multi_stacks.py
@@ -60,7 +60,7 @@ def main():
 
     print("Original Stack 1:",end=' ')
     stack1.print_stack()
-    print("Original Stack 2:",)
+    print("Original Stack 2:",end=' ')
 
     #Poping of top elements of both stacks
     print("Popped top element of stack 1:",stack1.pop())
