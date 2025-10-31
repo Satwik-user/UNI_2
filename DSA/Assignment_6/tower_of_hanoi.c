444a550ecb91cb81ab3fc90a42e21365e640a3df
Satwik-user <satwik2400@gmail.com>
2025-10-31 15:17:11 +0000

    Oct 31, 2025, 3:17 PM

--- DSA/Assignment_6/tower_of_hanoi.c
+++ DSA/Assignment_6/tower_of_hanoi.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
-    if (n == 0) {
+    if (n == 0) 
+    {
         return;
     }
     towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
