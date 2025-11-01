d053e4185111a79579a56335fa443860508ae361
Satwik-user <satwik2400@gmail.com>
2025-11-01 14:21:24 +0000

    Nov 1, 2025, 2:21 PM

--- DSA/Assignment_5/doubly.c
+++ DSA/Assignment_5/doubly.c
@@ -58,7 +58,7 @@ void insert_pos(Node **head, int data, int pos)
     }
     newNode->next=temp->next;
     newNode->prev=temp;
-    if(temp->next!=NULL)
+    if(temp->next!=NULL)P
     temp->next->prev=newNode;
     temp->next=newNode;
 }
