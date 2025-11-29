b2daa3fe57fb7702e8d18b794536b1d83672cc8c
Satwik-user <satwik2400@gmail.com>
2025-11-29 15:27:49 +0000

    Nov 29, 2025, 3:27 PM

--- DSA/Assignment_7/dequeue_arr.c
+++ DSA/Assignment_7/dequeue_arr.c
@@ -4,6 +4,7 @@ typedef struct Queue
 {
     int *arr;
     int cap;
+    int size;
     int front;
     int rear;
 }Queue;
@@ -12,9 +13,18 @@ Queue *createQueue(int cap)
 {
     Queue *queue=(Queue *)malloc(sizeof(Queue));
     queue->cap=cap;
+    queue->size=0;
     queue->front=queue->rear=-1;
     queue->arr=(int *)malloc(cap*sizeof(int));
     return queue;
 }
 
 int isEmpty(Queue *queue)
+{
+    return queue->front==-1;
+}
+
+int isFull(Queue *queue)
+{
+    return queue->size==queue->cap;
+}
