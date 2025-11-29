703d2febad965f2db2a081dc2a7e77c1945c2fa0
Satwik-user <satwik2400@gmail.com>
2025-11-29 15:16:13 +0000

    Nov 29, 2025, 3:16 PM

--- DSA/Assignment_7/dequeue_arr.c
+++ DSA/Assignment_7/dequeue_arr.c
@@ -13,4 +13,5 @@ Queue *createQueue(int cap)
     Queue *queue=(Queue *)malloc(sizeof(Queue));
     queue->cap=cap;
     queue->front=queue->rear=-1;
+    queue->arr=(int *)malloc(cap*sizeof(int))
 }
