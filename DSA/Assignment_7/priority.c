0df171d7702d73925f3dbe2234f996ba9b281222
Satwik-user <satwik2400@gmail.com>
2025-11-30 06:47:39 +0000

    Nov 30, 2025, 6:47 AM

---
+++ DSA/Assignment_7/priority.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    int pri;
+    struct Node *next;
+}Node;
+
+typedef struct Queue
+{
+    int cap;
+    int size;
+    struct Node *front;
+    struct Node *rear;
+}Queue;
+
+Node *createNode(int cap)
+{
+    
+}
