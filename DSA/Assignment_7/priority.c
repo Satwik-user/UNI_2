451319207bbd8900903a0f273d4d327aa5632b7d
Satwik-user <satwik2400@gmail.com>
2025-11-30 07:26:00 +0000

    Nov 30, 2025, 7:26 AM

--- DSA/Assignment_7/priority.c
+++ DSA/Assignment_7/priority.c
@@ -113,7 +113,11 @@ int main()
     
     Queue *queue=creaeteQueue(n);
 
-    //
+    //Formation of Queue
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter element and priority:");
+    }
     
     return 0;
 }
