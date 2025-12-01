f9efba41e8e85f5b9a8ae9ae1c607e41a3e46191
Satwik-user <satwik2400@gmail.com>
2025-12-01 05:38:40 +0000

    Dec 1, 2025, 5:38 AM

--- DSA/Assignment_7/multiple.c
+++ DSA/Assignment_7/multiple.c
@@ -88,8 +88,5 @@ int peekA(Queue *queue)
         printf("Queue is empty!\n");
         return -1;
     }
-    for(int i=queue->frontA;i<=queue->rearB;i++)
-    printf("%d ",queue->arr[i]);
-
     
 }
