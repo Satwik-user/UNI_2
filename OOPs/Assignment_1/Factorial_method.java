ca791496c9f9a7902ef313d8610a65ad5fa28d8a
Satwik-user <satwik2400@gmail.com>
2026-01-10 15:44:09 +0000

    Jan 10, 2026, 3:44 PM

--- OOPs/Assignment_1/Factorial_method.java
+++ OOPs/Assignment_1/Factorial_method.java
@@ -11,7 +11,7 @@ class Factorial_method
     public static void main(String args[])
     {
         int n = 5;
-        int factFactorial_method.factorial(n);
+        int fact = Factorial_method.factorial(n);
         Sys
     }
 }
