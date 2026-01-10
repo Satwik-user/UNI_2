916803cd997d0056933ecbfd35ed90d27edccb1e
Satwik-user <satwik2400@gmail.com>
2026-01-10 15:31:50 +0000

    Jan 10, 2026, 3:31 PM

--- OOPs/Assignment_1/Factorial.java
+++ OOPs/Assignment_1/Factorial.java
@@ -3,7 +3,7 @@ class Factorial
     public static void main(String args[])
     {
         int n = 5, f = 1;
-        for(int i=0; i<n; i++)
+        for(int i=1; i<n; i++)
         f *= i;
         System.out.println("Factorial = " + f);
     }
