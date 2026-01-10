207efdf83fd90580552de764b1e2fdc2f45cd664
Satwik-user <satwik2400@gmail.com>
2026-01-10 15:28:26 +0000

    Jan 10, 2026, 3:28 PM

--- OOPs/Assignment_1/Factorial.java
+++ OOPs/Assignment_1/Factorial.java
@@ -5,6 +5,6 @@ class Factorial
         int n = 5, f = 1;
         for(int i=0; i<n; i++)
         f *= i;
-        System.out.println("Factorial of the number")
+        System.out.println("Factorial = ")
     }
 }
