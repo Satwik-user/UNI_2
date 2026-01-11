4a169d31c4e28fa0730b4806e9947c86837e3870
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:34:11 +0000

    Jan 11, 2026, 11:34 AM

--- OOPs/Assignment_1/Grade.java
+++ OOPs/Assignment_1/Grade.java
@@ -5,6 +5,6 @@ class Grade
     public static vois main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ")
+        System.out.print("Enter marks:")
     }
 }
