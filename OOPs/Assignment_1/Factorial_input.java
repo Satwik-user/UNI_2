8e2fe93a68720f2e7de614ad8d6d5b86cb9a335d
Satwik-user <satwik2400@gmail.com>
2026-01-11 05:35:21 +0000

    Jan 11, 2026, 5:35 AM

--- OOPs/Assignment_1/Factorial_input.java
+++ OOPs/Assignment_1/Factorial_input.java
@@ -15,7 +15,7 @@ class Factorial_input
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number:");
+        System.out.print("Enter a number:");
         int n = sc.nextInt();
 
         int fact = factorial(n);
