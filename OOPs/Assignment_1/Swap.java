244cdabacce490a1eb2a079928e1acb7125e2875
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:10:32 +0000

    Jan 11, 2026, 11:10 AM

--- OOPs/Assignment_1/Swap.java
+++ OOPs/Assignment_1/Swap.java
@@ -12,7 +12,7 @@ class Swap
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
+        int  = sc.nextInt();
         int n2 = sc.nextInt();
 
         swap(n1, n2);
