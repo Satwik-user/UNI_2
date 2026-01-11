2167e29bf81590d7f83aaa1624dca89678ad8150
Satwik-user <satwik2400@gmail.com>
2026-01-11 06:36:02 +0000

    Jan 11, 2026, 6:36 AM

--- OOPs/Assignment_1/Max_three.java
+++ OOPs/Assignment_1/Max_three.java
@@ -11,9 +11,9 @@ class Max_three
         int c = sc.nextInt();
 
         if(a > b && a > c)
-        System.out.println(a + "is the maximum number");
+        System.out.println(a + " is the maximum number");
         else if(b > a && b > c)
-        System.out.println(b + "is the maximum number");
+        System.out.println(b + " is the maximum number");
         else
         System.out.println(c + "is the maximum number");
     }
