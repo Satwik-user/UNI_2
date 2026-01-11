6682e932a1dbcee9fdf4cd5e9dd5ad0ca7e97730
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:14:54 +0000

    Jan 11, 2026, 11:14 AM

--- OOPs/Assignment_1/Swap.java
+++ OOPs/Assignment_1/Swap.java
@@ -12,7 +12,7 @@ class Swap
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print()
+        System.out.print("Enter ")
         int a = sc.nextInt();
         int b = sc.nextInt();
 
