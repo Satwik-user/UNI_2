f53d24d541cae56760ab60953e21c79360d82ec9
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:15:01 +0000

    Jan 11, 2026, 11:15 AM

--- OOPs/Assignment_1/Swap.java
+++ OOPs/Assignment_1/Swap.java
@@ -12,7 +12,7 @@ class Swap
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter two numbers ot swap")
+        System.out.print("Enter two numbers ot swap:");
         int a = sc.nextInt();
         int b = sc.nextInt();
 
