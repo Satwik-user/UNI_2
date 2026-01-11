45fed0d6dae41a853a624f241c569f0abf5e3ca2
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:15:23 +0000

    Jan 11, 2026, 11:15 AM

--- OOPs/Assignment_1/Swap.java
+++ OOPs/Assignment_1/Swap.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 class Swap
 {
-    public static void swap(int a, int b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
