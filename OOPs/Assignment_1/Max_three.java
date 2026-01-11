6b0c526782e66d3c6fe3fdb5975527aba1f08deb
Satwik-user <satwik2400@gmail.com>
2026-01-11 06:30:26 +0000

    Jan 11, 2026, 6:30 AM

--- OOPs/Assignment_1/Max_three.java
+++ OOPs/Assignment_1/Max_three.java
@@ -5,6 +5,6 @@ class Max_three
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter ")
+        System.out.println("Enter a number:");
     }
 }
