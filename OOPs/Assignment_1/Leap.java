6fe2be792bf578594c7ba040965eb4ea0f5a25c6
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:31:47 +0000

    Jan 11, 2026, 11:31 AM

--- OOPs/Assignment_1/Leap.java
+++ OOPs/Assignment_1/Leap.java
@@ -11,6 +11,6 @@ class Leap
         if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         System.out.println("Leap Year!");
         else
-        
+        System.out.println("")
     }
 }
