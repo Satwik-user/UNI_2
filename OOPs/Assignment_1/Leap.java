26988bf1c8d500a95d68075e72abbf2116dcd1fd
Satwik-user <satwik2400@gmail.com>
2026-01-11 11:20:23 +0000

    Jan 11, 2026, 11:20 AM

--- OOPs/Assignment_1/Leap.java
+++ OOPs/Assignment_1/Leap.java
@@ -5,8 +5,9 @@ class Leap
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println()
         int year = sc.nextInt();
 
-        
+
     }
 }
