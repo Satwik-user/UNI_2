8aa55deaa71f6f0a64ae47591412086f2110933a
Satwik-user <satwik2400@gmail.com>
2026-01-11 05:32:05 +0000

    Jan 11, 2026, 5:32 AM

---
+++ OOPs/Assignment_1/Factorial_input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Factorial_input
+{
+    public static int factorial(int n)
+    {
+        int f = 1;
+        for(int i=1; i<=n; i++)
+        f *= i;
+
+        return f;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a ")
+    }
+}
