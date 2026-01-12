d912fc26a6fe0ed49c9d55fa8db42044be692f5c
Satwik-user <satwik2400@gmail.com>
2026-01-12 14:43:16 +0000

    Jan 12, 2026, 2:43 PM

--- OOPs/Assignment_1/Grade.java
+++ OOPs/Assignment_1/Grade.java
@@ -11,7 +11,7 @@ class Grade
         if(marks >= 90)
         System.out.println("Grade A");
         else if(marks >=80 && marks < 90)
-        System.out.print("Grade B");
+        System.out.println("Grade B");
         else if(marks >= 70 && marks < 80)
         System.out.println("Grade C");
         else if(marks >= 60 && marks < 70)
