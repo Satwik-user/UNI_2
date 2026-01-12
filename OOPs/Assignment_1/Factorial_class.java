4ab5b13b52d7331be2ae01fcd53c88f6c33767f0
Satwik-user <satwik2400@gmail.com>
2026-01-12 14:08:49 +0000

    Jan 12, 2026, 2:08 PM

--- OOPs/Assignment_1/Factorial_class.java
+++ OOPs/Assignment_1/Factorial_class.java
@@ -15,7 +15,7 @@ class Factorial_class
     public static void main(String args[])
     {
         int n = 5;
-        Factorial_main obj = new Factorial_main();
+        Factorial_calc obj = new Factorial_main();
         int fact = obj.factorial(n);
         System.out.println("Factorial = " + fact);
     }
