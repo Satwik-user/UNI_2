2df8e9212a526ec9b7d9d5f88ab8c9e8ed8b3114
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:52:01 +0000

    Jan 19, 2026, 2:52 PM

--- OOPs/Assignment_2/Sum_2d.java
+++ OOPs/Assignment_2/Sum_2d.java
@@ -5,8 +5,8 @@ class Sum_2d
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of rows and columns(Array 1):");
-        int n1 = sc.nextInt();
-        int m1 = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
         int arr1[][] = new int[n1][m1];
         System.out.println("Enter array 1 elements:");
