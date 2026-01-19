39358f00d111d9ca037fbb1628b794a636c2e598
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:56:01 +0000

    Jan 19, 2026, 2:56 PM

--- OOPs/Assignment_2/Sum_2d.java
+++ OOPs/Assignment_2/Sum_2d.java
@@ -4,7 +4,7 @@ class Sum_2d
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of rows and columns(Array 1):");
+        System.out.println("Enter size of rows and columns:");
         int n = sc.nextInt();
         int m = sc.nextInt();
 
