d2da4a36735cfaf45bbf8d7d22b4b08e59cad681
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:10:50 +0000

    Jan 19, 2026, 3:10 PM

--- OOPs/Assignment_2/Matrix_multi.java
+++ OOPs/Assignment_2/Matrix_multi.java
@@ -19,6 +19,8 @@ class Matrix_multi
         int arr2[][] = new int[n][m];
         System.out.println("Enter array 2 elements:");
         for(int i=0; i<n; i++)
-        
+        {
+            for()
+        }
     }
 }
