792c939ccdb316cc91106f02d657895fcf7b92c3
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:42:34 +0000

    Jan 19, 2026, 2:42 PM

--- OOPs/Assignment_2/Sum_even.java
+++ OOPs/Assignment_2/Sum_even.java
@@ -18,6 +18,6 @@ class Sum_even
             if(arr[i] % 2 == 0)
             sum += arr[i];
         }
-        System.out.pritnln("Sum of even integers:")
+        System.out.pritnln("Sum of even integers:" + sum);
     }
 }
