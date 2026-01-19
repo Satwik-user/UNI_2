495b1d19437da6b32626a110a840c243a61979eb
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:27:36 +0000

    Jan 19, 2026, 3:27 PM

--- OOPs/Assignment_2/default_construct.java
+++ OOPs/Assignment_2/default_construct.java
@@ -4,5 +4,6 @@ class default_construct
     {
         System.out.println("Factorial_calc object created");
     }
-    public static void main()
+    public static void main(String args[])
+    
 }
