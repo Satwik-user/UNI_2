bc7b5af671842400c1a44e69774d75aedd44e700
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:48:54 +0000

    Jan 19, 2026, 2:48 PM

--- OOPs/Assignment_2/Sum_2d.java
+++ OOPs/Assignment_2/Sum_2d.java
@@ -16,6 +16,6 @@ class Sum_2d
             arr[i][j] = sc.nextInt();
         }
 
-        System.out.pritnln("Enter size of rows and columns(a)")
+        System.out.pritnln("Enter size of rows and columns(Array 2)")
     }
 }
