fd955755c0248ba004427e3a1697726ff8fb346f
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:17:20 +0000

    Jan 19, 2026, 3:17 PM

--- OOPs/Assignment_2/Matrix_multi.java
+++ OOPs/Assignment_2/Matrix_multi.java
@@ -39,7 +39,7 @@ class Matrix_multi
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
-            System.out.print
+            System.out.print("")
         }
     }
 }
