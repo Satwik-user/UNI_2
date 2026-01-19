ed577b3073ba9dac7da068cbcce644d268341f82
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:54:08 +0000

    Jan 19, 2026, 2:54 PM

--- OOPs/Assignment_2/Sum_2d.java
+++ OOPs/Assignment_2/Sum_2d.java
@@ -31,6 +31,7 @@ class Sum_2d
             sum[i][j] = arr1[i][j] + arr2[i][j];
         }
 
-        System.out.println("Sum of ")
+        System.out.println("Sum of two arrays:");
+        for()
     }
 }
