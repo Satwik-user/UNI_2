1eefbe46e91c8c9078733d590b7929bacf36efbb
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:13:08 +0000

    Jan 19, 2026, 2:13 PM

--- OOPs/Assignment_2/array.java
+++ OOPs/Assignment_2/array.java
@@ -5,7 +5,7 @@ class array
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("")
+        System.out.print("Enter ")
         int n = sc.nextInt();
 
         for(int i=0; i<n; i++)
