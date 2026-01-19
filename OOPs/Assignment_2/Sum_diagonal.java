2d8a4f9579db297ae81482b2b0e000ac5b46a592
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:02:08 +0000

    Jan 19, 2026, 3:02 PM

--- OOPs/Assignment_2/Sum_diagonal.java
+++ OOPs/Assignment_2/Sum_diagonal.java
@@ -25,6 +25,6 @@ class Sum_diagonal
                 sum += arr[i][j];
             }
         }
-        System.out.println("Sum of diagonal elements:")
+        System.out.println("Sum of diagonal elements:" + sum);
     }
 }
