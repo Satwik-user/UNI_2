f4052ee12396b51b0c74a176971c52cdaf0d77ef
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:17:19 +0000

    Jan 19, 2026, 3:17 PM

--- OOPs/Assignment_2/Matrix_multi.java
+++ OOPs/Assignment_2/Matrix_multi.java
@@ -38,7 +38,8 @@ class Matrix_multi
         System.out.println("Multiplied array:");
         for(int i=0; i<m; i++)
         {
-            for(int j=0; j<n;)
+            for(int j=0; j<n; j++)
+            System.out.print
         }
     }
 }
