0be3cff1a7f6ce1770bc38ac888b6b8ebbc4b82a
Satwik-user <satwik2400@gmail.com>
2026-01-19 15:00:01 +0000

    Jan 19, 2026, 3:00 PM

--- OOPs/Assignment_2/Sum_diagonal.java
+++ OOPs/Assignment_2/Sum_diagonal.java
@@ -12,7 +12,8 @@ class Sum_diagonal
         System.out.println("Enter array elements:");
         for(int i=0; i<n; i++)
         {
-            for()
+            for(int j=0; j<m; j++)
+            
         }
     }
 }
