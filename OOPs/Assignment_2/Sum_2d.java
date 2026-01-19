01e25c6cf6ac8dadfd624164a5c96f3fce3692a0
Satwik-user <satwik2400@gmail.com>
2026-01-19 14:52:12 +0000

    Jan 19, 2026, 2:52 PM

--- OOPs/Assignment_2/Sum_2d.java
+++ OOPs/Assignment_2/Sum_2d.java
@@ -12,7 +12,7 @@ class Sum_2d
         System.out.println("Enter array 1 elements:");
         for(int i=0; i<n; i++)
         {
-            for(int j=0; j<m1; j++)
+            for(int j=0; j<m; j++)
             arr1[i][j] = sc.nextInt();
         }
 
