c057e73c71ce4cc01170131a4a41222fff39fe62
Satwik-user <satwik2400@gmail.com>
2026-01-29 15:09:21 +0000

    Jan 29, 2026, 3:09 PM

--- DAA/Assignment_1/binary_search.py
+++ DAA/Assignment_1/binary_search.py
@@ -1,6 +1,6 @@
 n = int(input("Enter number of elements:"))
 search_ele = int(input("Enter search element:"))
 arr = []
-
+print("Enter sorted elements")
 for i in range(n):
     arr.append(int(input("Enter element:")))
