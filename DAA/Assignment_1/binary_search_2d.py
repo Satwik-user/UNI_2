9516f316574b9aa428ec1c05bc2b8c81f7ccbdd6
Satwik-user <satwik2400@gmail.com>
2026-01-29 15:52:12 +0000

    Jan 29, 2026, 3:52 PM

--- DAA/Assignment_1/binary_search_2d.py
+++ DAA/Assignment_1/binary_search_2d.py
@@ -3,4 +3,7 @@ col = int(input("Enter number of columns:"))
 
 search_ele = int(input("Enter search element:"))
 
-arr = [row][]
+arr = [row][col]
+for i in range(row):
+    for j in range(col):
+        
