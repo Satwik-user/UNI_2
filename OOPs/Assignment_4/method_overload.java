4e77f261fdce5d14d9d6a1565ec2ccd8d8ed5499
Satwik-user <satwik2400@gmail.com>
2026-02-01 14:05:31 +0000

    Feb 1, 2026, 2:05 PM

--- OOPs/Assignment_4/method_overload.java
+++ OOPs/Assignment_4/method_overload.java
@@ -1,8 +1,8 @@
 class Method_overload
 {
-    public void add(int a, )
+    public void add(int a)
     {
-        this.
+        
         System.out.println("Ink colour is " + )
     }
     public static void main(String args[])
