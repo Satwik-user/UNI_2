c5e1c4169a45c75f6461b6d1c96047a763863112
Satwik-user <satwik2400@gmail.com>
2026-02-01 14:07:15 +0000

    Feb 1, 2026, 2:07 PM

--- OOPs/Assignment_4/method_overload.java
+++ OOPs/Assignment_4/method_overload.java
@@ -7,7 +7,7 @@ class Method_overload
 
     public void add(int a, int b, int c)
     {
-        System.out.println("Sum: " + ())
+        System.out.println("Sum: " + (a + b + c))
     }
     public static void main(String args[])
     {
