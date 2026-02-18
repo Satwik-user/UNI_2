85b0d5c326df33f7f215e6a1a8c27be35ea71c12
Satwik-user <satwik2400@gmail.com>
2026-02-18 15:21:25 +0000

    Feb 18, 2026, 3:21 PM

--- DAA/bitonic_sequence.c
+++ DAA/bitonic_sequence.c
@@ -11,6 +11,12 @@ int main()
         scanf("%d", &arr[i]);
     }
     
+    int bitonic = 0;
+    for(int i=0; i<n-1; i++)
+    {
+        if(arr[i] < arr[i+1])
+        continue;
+    }
 
     return 0;
 }
