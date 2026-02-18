0ef748e27c17d9703549f33a57c2fd9f6c587d4e
Satwik-user <satwik2400@gmail.com>
2026-02-18 15:30:49 +0000

    Feb 18, 2026, 3:30 PM

--- DAA/bitonic_sequence.c
+++ DAA/bitonic_sequence.c
@@ -11,14 +11,14 @@ int main()
         scanf("%d", &arr[i]);
     }
     
-    int bitonic = 0;
+    int bitonic_point = -1;
     for(int i=0; i<n-1; i++)
     {
         if(arr[i] < arr[i+1])
         continue;
         else
         {
-            
+            bitonic_point = i;
         }
     }
 
