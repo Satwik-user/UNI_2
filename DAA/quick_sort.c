ecebb5000666134a04cb3684f0945fe6df0fdcfd
Satwik-user <satwik2400@gmail.com>
2026-02-19 17:05:35 +0000

    Feb 19, 2026, 5:05 PM

--- DAA/quick_sort.c
+++ DAA/quick_sort.c
@@ -11,7 +11,7 @@ void swap(int *a, int *b)
 int partition(int arr[], int l, int r)
 {
     int pivot = l;
-    int i = l, j = r;
+    int i = l + 1, j = r;
     while(i < j)
     {
         while(i <= r)
