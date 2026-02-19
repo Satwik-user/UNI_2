cb2fbf0cb25d8d096595e90adcc37d43ad99f6f7
Satwik-user <satwik2400@gmail.com>
2026-02-19 17:22:14 +0000

    Feb 19, 2026, 5:22 PM

--- DAA/occurence.c
+++ DAA/occurence.c
@@ -7,6 +7,10 @@ int main()
 
     int *arr = (int *)malloc(n * aizeof(n));
     for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", arr[i]);
+    }
     
     return 0;
 }
