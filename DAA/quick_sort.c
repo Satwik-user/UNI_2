036b78b844abea016bbe899b6479cbda4e913048
Satwik-user <satwik2400@gmail.com>
2026-02-19 16:42:35 +0000

    Feb 19, 2026, 4:42 PM

--- DAA/quick_sort.c
+++ DAA/quick_sort.c
@@ -38,9 +38,13 @@ int main()
     printf("Enter size of array:");
     scanf("%d", &n);
 
-    int arr = (int *)malloc(n * sizeof(int));
+    int *arr = (int *)malloc(n * sizeof(int));
 
-    for(int i=0; i)
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", &arr[i]);
+    }
     
     return 0;
 }
