28d6eaea1f2ff1a7181457bef45b27048bc58323
Satwik-user <satwik2400@gmail.com>
2026-02-19 15:49:45 +0000

    Feb 19, 2026, 3:49 PM

--- DAA/inversion_pairs.c
+++ DAA/inversion_pairs.c
@@ -47,3 +47,18 @@ long merge_sort_count(int arr[], int l, int r)
         return inv;
     }
 }
+int main()
+{
+    int n;
+    printf("Enter size of array:");
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", &arr[i]);
+    }
+    
+    return 0;
+}
