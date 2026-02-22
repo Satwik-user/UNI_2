b531679b39fcd5630cdfb1e10ab1337266b12a46
Satwik-user <satwik2400@gmail.com>
2026-02-22 07:51:33 +0000

    Feb 22, 2026, 7:51 AM

--- OOPs/Assignment_5/Static_keyword.java
+++ OOPs/Assignment_5/Static_keyword.java
@@ -1,9 +1,4 @@
 class Static_implement
 {
     public static String university = "UEM";
-    public static void main(String args[])
-    {
-        university = "IEM";
-        System.out.println(university);
-    }
 }
