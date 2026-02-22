726518d821f8002d8ff7faeea444162dcc026495
Satwik-user <satwik2400@gmail.com>
2026-02-22 18:40:40 +0000

    Feb 22, 2026, 6:40 PM

--- OOPs/Assignment_5/Circle_point.java
+++ OOPs/Assignment_5/Circle_point.java
@@ -12,7 +12,7 @@ class Circle extends Point
 {
     double area()
     {
-        return Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - y2), 2);
+        return Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - this.y2), 2);
     }
 }
 
@@ -20,6 +20,6 @@ class Circle_point
 {
     public static void main(String args[])
     {
-
+        
     }
 }
