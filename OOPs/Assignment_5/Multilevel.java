ce3c67b4363f57331420e861b077d70f3dfbd11e
Satwik-user <satwik2400@gmail.com>
2026-02-22 17:55:59 +0000

    Feb 22, 2026, 5:55 PM

--- OOPs/Assignment_5/Multilevel.java
+++ OOPs/Assignment_5/Multilevel.java
@@ -10,7 +10,7 @@ class Child_1 extends Base
     void child_display_1()
     {
         Base b = new Base();
-        System.out.println("#1 level of inheritance of ");
+        System.out.print("#1 level of inheritance of ");
         b.display();
     }
 }
