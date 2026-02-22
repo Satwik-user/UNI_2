c91e4526ca7defe0014b88b44e4c6ee3eab17ce8
Satwik-user <satwik2400@gmail.com>
2026-02-22 19:03:34 +0000

    Feb 22, 2026, 7:03 PM

--- OOPs/Assignment_5/Employee_details.java
+++ OOPs/Assignment_5/Employee_details.java
@@ -3,7 +3,7 @@ class Employee
 {
     String emp_id, emp_name;
     double basic_salary;
-    void Employee(String emp_id, int emp_name, int basic_salary)
+    void Employee(String emp_id, String emp_name, idoublent basic_salary)
     {
         this.emp_id = emp_id;
         this.emp_name = emp_name;
