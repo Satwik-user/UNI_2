dd4d4a9d55e795ca1485716c1028f75994049963
Satwik-user <satwik2400@gmail.com>
2026-02-22 05:19:29 +0000

    Feb 22, 2026, 5:19 AM

--- DAA/2nd_large_small.c
+++ DAA/2nd_large_small.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 typedef struct
 {
     int max, secondMax;
@@ -42,6 +44,54 @@ Result secondExtreme(int arr[], int l, int r)
     if(l == r)
     {
         res.max = res.min = arr[l];
-        
+        res.secondMax = INT_MIN;
+        res.secondMin = INT_MAX;
+        return res;
     }
+    if(r - l == 1)
+    {
+        if(arr[l] > arr[r])
+        {
+            res.max = arr[l];
+            res.secondMax = arr[r];
+            res.min = arr[r];
+            res.secondMin = arr[l];
+        }
+        else
+        {
+            res.max = arr[r];
+            res.secondMax = arr[l];
+            res.min = arr[l];
+            res.secondMin = arr[r];
+        }
+        return res;
+    }
+
+    int m = l + (r - l) / 2;
+
+    Result left = secondExtreme(arr, l, m);
+    Result right = secondExtreme(arr, m+1, r);
+
+    return merge(left, right);
+}
+
+int main()
+{
+    int n;
+    printf("Enter size of array:");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", &arr[i]);
+    }
+
+    Result res = secondExtreme(arr, 0, n-1);
+
+    printf("Second Largest element:%d\n", res.secondMax);
+    printf("Second smallest element:%d\n", res.secondMin);
+    
+    return 0;
 }
