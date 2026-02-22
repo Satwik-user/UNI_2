305e120ba03c1dfdbadaff7d079caddb9820440c
Satwik-user <satwik2400@gmail.com>
2026-02-22 18:57:25 +0000

    Feb 22, 2026, 6:57 PM

--- OOPs/Assignment_5/Employee_details.java
+++ OOPs/Assignment_5/Employee_details.java
@@ -1,3 +1,4 @@
+
 class Employee
 {
     String emd_id, emp_name;
@@ -19,6 +20,6 @@ class Employee_salary
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        
+
     }
 }
