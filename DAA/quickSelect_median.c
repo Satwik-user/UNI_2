69d380e677fb339ae072cd3a21d7616a98809096
Satwik-user <satwik2400@gmail.com>
2026-02-22 06:57:20 +0000

    Feb 22, 2026, 6:57 AM

--- DAA/quickSelect_median.c
+++ DAA/quickSelect_median.c
@@ -60,6 +60,7 @@ int main()
     printf("Median element:%d\n", arr[median]);
     if(median - 1 >= 0)
     printf("Left Neighbour:%d\n", arr[median - 1]);
+    if(median + 1 <= n)
     printf("Right Neighbour:%d\n", arr[median + 1]);
     
     return 0;
