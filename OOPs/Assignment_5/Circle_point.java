efd2e1d133261484c55d7db61bdab55e314cc68c
Satwik-user <satwik2400@gmail.com>
2026-02-22 18:42:59 +0000

    Feb 22, 2026, 6:42 PM

--- OOPs/Assignment_5/Circle_point.java
+++ OOPs/Assignment_5/Circle_point.java
@@ -22,6 +22,7 @@ class Circle_point
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        
         Point p = new Point();
         System.out.println("Enter coordinates of centre and point on circumference:");
 
