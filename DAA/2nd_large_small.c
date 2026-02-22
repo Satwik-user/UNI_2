2aa5262aed118b9c61163bc3c47387110b305518
Satwik-user <satwik2400@gmail.com>
2026-02-22 05:02:23 +0000

    Feb 22, 2026, 5:02 AM

--- DAA/2nd_large_small.c
+++ DAA/2nd_large_small.c
@@ -22,6 +22,6 @@ Result merge(Result left, Result right)
     if(left.min < right.min)
     {
         res.min = left.min;
-        res.secon
+        res.secondMin = (left.secondMin < right.min)? left.secondMin: right.min;
     }
 }
