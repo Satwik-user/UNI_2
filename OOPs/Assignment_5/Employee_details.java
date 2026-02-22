a6057723d8618d3129dd1cba7b43d05593f900c6
Satwik-user <satwik2400@gmail.com>
2026-02-22 19:00:04 +0000

    Feb 22, 2026, 7:00 PM

--- OOPs/Assignment_5/Employee_details.java
+++ OOPs/Assignment_5/Employee_details.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Employee
 {
-    String emd_id, emp_name;
+    String emp_id, emp_name;
     double basic_salary;
     void Employee(emp_id, emp_name, basic_salary)
     {
