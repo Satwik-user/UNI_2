1002c58c64f5590f165f4ebd607d33c491af0730
Satwik-user <satwik2400@gmail.com>
2026-02-22 16:23:14 +0000

    Feb 22, 2026, 4:23 PM

--- OOPs/Assignment_5/Hierarchical.java
+++ OOPs/Assignment_5/Hierarchical.java
@@ -28,6 +28,7 @@ class Heirarchical
     public static void main(String args[])
     {
         Child_1 c1 = new Child_1();
-        Child_2 c2 = new Child_2
+        Child_2 c2 = new Child_2();
+        
     }
 }
