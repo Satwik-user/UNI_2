49b6aef8df78af7b7361b1f77510f3abf38ab3f2
Satwik-user <satwik2400@gmail.com>
2026-02-22 18:52:38 +0000

    Feb 22, 2026, 6:52 PM

--- OOPs/Assignment_5/Employee_details.java
+++ OOPs/Assignment_5/Employee_details.java
@@ -2,8 +2,8 @@ class Employee
 {
     String emd_id, emp_name;
     double basic_salary;
-    void iput(emp_id, emp_name, basic_salary)
+    void Employee(emp_id, emp_name, basic_salary)
     {
-
+        
     }
 }
