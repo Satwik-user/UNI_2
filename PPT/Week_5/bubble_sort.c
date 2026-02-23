ee8099e8fdea546de71a339ce6f5af8c46337cc9
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:10:59 +0000

    Feb 23, 2026, 6:10 AM

--- PPT/Week_5/bubble_sort.c
+++ PPT/Week_5/bubble_sort.c
@@ -6,6 +6,12 @@ int main()
     printf("Enter array size:");
     scanf("%d", &n);
 
+    int *arr = (int *)malloc(n * sizeof(int));
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", &arr[i]);
+    }
     
     
     return 0;
