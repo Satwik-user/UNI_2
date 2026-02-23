bc7bed95495d1b4988cbedcc1e046d4155327a19
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:47:57 +0000

    Feb 23, 2026, 6:47 AM

--- OOPs/Assignment_5/Fraction.java
+++ OOPs/Assignment_5/Fraction.java
@@ -8,10 +8,5 @@ class Fraction
 
 class Fraction_implement
 {
-    public static void main(String args[])
-    {
-        Fraction f1 = new Fraction(2, 3);
-        Fraction f2 = new Fraction(3, 4);
-        Fraction f3 = new Fraction(4, 5);
-    }
+
 }
