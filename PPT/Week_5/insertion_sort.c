b3334f46c17fc8baa5efe1a2f791989e3b4eec60
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:28:25 +0000

    Feb 23, 2026, 6:28 AM

--- PPT/Week_5/insertion_sort.c
+++ PPT/Week_5/insertion_sort.c
@@ -7,7 +7,11 @@ int main()
     scanf("%d", &n);
 
     int *arr = (int *)malloc(n * sizeof(int));
-    
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element:");
+        scanf("%d", &arr[i]);
+    }
     
     return 0;
 }
