6cee2da381d80d17156b1e36f0da46313a271865
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:47:58 +0000

    Feb 23, 2026, 6:47 AM

--- OOPs/Assignment_5/Fraction.java
+++ OOPs/Assignment_5/Fraction.java
@@ -4,6 +4,12 @@ class Fraction
     {
         System.out.println("Fraction:" + numerator / denominator);
     }
+        public static void main(String args[])
+    {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(3, 4);
+        Fraction f3 = new Fraction(4, 5);
+    }
 }
 
 class Fraction_implement
