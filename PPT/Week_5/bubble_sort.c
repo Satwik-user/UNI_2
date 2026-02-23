c5918a3172c19ce1f4d1959537a0ac6d60fdbf99
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:13:22 +0000

    Feb 23, 2026, 6:13 AM

--- PPT/Week_5/bubble_sort.c
+++ PPT/Week_5/bubble_sort.c
@@ -17,7 +17,12 @@ int main()
     {
         for(int j=0; j<i-1; j++)
         {
-            
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
     
