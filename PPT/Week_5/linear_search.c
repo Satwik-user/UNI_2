dfda673f61756742363167d6f72fb699e518de3f
Satwik-user <satwik2400@gmail.com>
2026-02-23 05:55:44 +0000

    Feb 23, 2026, 5:55 AM

--- PPT/Week_5/linear_search.c
+++ PPT/Week_5/linear_search.c
@@ -26,9 +26,9 @@ int main()
     }
 
     if(f == 1)
-    printf("Element found in array!");
+    printf("Element found in array!\n");
     else
-    printf("Element not found in array");
+    printf("Element not found in array\n");
     
     return 0;
 }
