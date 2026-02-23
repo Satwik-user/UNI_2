6c26ef2b189457106af495d622f260903e3117a9
Satwik-user <satwik2400@gmail.com>
2026-02-23 06:22:20 +0000

    Feb 23, 2026, 6:22 AM

--- PPT/Week_5/selection_sort.c
+++ PPT/Week_5/selection_sort.c
@@ -16,7 +16,10 @@ int main()
     for(int i=0; i<n-1; i++)
     {
         int min_idx = i;
-        
+        for(int j=i+1; j<n; j++)
+        {
+            
+        }
     }
     
     return 0;
