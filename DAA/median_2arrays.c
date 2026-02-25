1a13653973c93837fa98f8f04e0a42e0a7c033cb
Satwik-user <satwik2400@gmail.com>
2026-02-25 12:19:41 +0000

    Feb 25, 2026, 12:19 PM

--- DAA/median_2arrays.c
+++ DAA/median_2arrays.c
@@ -3,7 +3,7 @@
 
 void merge(int L[], int R[], int l, int r)
 {
-    int *arr = (int *)malloc((l + r) * sizeof(int))
+    int *arr = (int *)malloc((l + r) * sizeof(int));
     int i = 0, j = 0, k = 0;
     while(i < l && j < r)
     {
