716d8f6e78f1f1db9b7a06e5fbcefc56ca3e9571
Satwik-user <satwik2400@gmail.com>
2026-02-25 12:49:31 +0000

    Feb 25, 2026, 12:49 PM

---
+++ DAA/bucket_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void bucketSort(int arr[], int n) {
+    int bucket[10] = {0};
+    
+    for(int i = 0; i < n; i++)
+        bucket[arr[i]]++;
+    
+    int index = 0;
+    for(int i = 0; i < 10; i++)
+        for(int j = 0; j < bucket[i]; j++)
+            arr[index++] = i;
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 9, 2, 7, 3, 5, 1};
+    int n = 10;
+    
+    printf("Original: ");
+    for(int i = 0; i < n; i++) printf("%d ", arr[i]);
+    
+    bucketSort(arr, n);
+    
+    printf("\nSorted: ");
+    for(int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    
+    return 0;
+}
