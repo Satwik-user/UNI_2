e387d835678763568ae55d9dfbee2188c434f89d
Satwik-user <satwik2400@gmail.com>
2026-02-25 12:23:15 +0000

    Feb 25, 2026, 12:23 PM

--- DAA/median_2arrays.c
+++ DAA/median_2arrays.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge(int L[], int R[], int l, int r)
+void merge(int L[], int R[], int n)
 {
-    int *arr = (int *)malloc((l + r) * sizeof(int));
+    int *arr = (int *)malloc(2*n * sizeof(int));
 
     int i = 0, j = 0, k = 0;
-    while(i < l && j < r)
+    while(i < n && j < n)
     {
         if(L[i] < R[j])
         arr[k++] = L[i++];
         else
         arr[k++] = L[j++];
     }
-    
-    while(i < l)
-    arr[k++] = L[i++];
-
-    while(j < r)
-    arr[k++] = R[j++];
 }
 
 int median(int arr[])
 {
-    
+
 }
