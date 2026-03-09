980ca6926248364dea5a55cde368a8273b5d53d4
Satwik-user <satwik2400@gmail.com>
2026-03-09 14:39:20 +0000

    Mar 9, 2026, 2:39 PM

---
+++ OOPs/Assignment_6/P5_MultipleInheritanceInterface.java
@@ -0,0 +1,15 @@
+interface Flyable { void fly(); }
+interface Swimmable { void swim(); }
+
+class Duck implements Flyable, Swimmable {
+    public void fly() { System.out.println("Duck flies"); }
+    public void swim() { System.out.println("Duck swims"); }
+}
+
+public class P5_MultipleInheritanceInterface {
+    public static void main(String args[]) {
+        Duck d = new Duck();
+        d.fly();
+        d.swim();
+    }
+}
