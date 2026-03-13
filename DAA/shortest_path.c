67bd2389649231501d7af0a1e1cbe092bef40fce
Satwik-user <satwik2400@gmail.com>
2026-03-13 04:45:39 +0000

    Mar 13, 2026, 4:45 AM

--- DAA/shortest_path.c
+++ DAA/shortest_path.c
@@ -5,9 +5,10 @@ int main()
     printf("Enter size of rows and columns:");
     scanf("%d %d", &row, &col);
 
+    int dist[row][col];
     for(int i=0; i<row; i++)
     for(int j=0; j<col; j++)
-    
+
 
     return 0;
 }
