d7229e6eaebc272bf0e33bf63eb5e6556e8ae7d9
Satwik-user <satwik2400@gmail.com>
2026-05-01 00:18:07 +0000

    May 1, 2026, 12:18 AM

--- DAA/Merge_sort.java
+++ DAA/Merge_sort.java
@@ -47,7 +47,7 @@ class Merge_sort
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the size of array:");
+        System.out.print("Enter the size of array:");
         int n = sc.nextInt();
 
         int arr[] = new int[n];
