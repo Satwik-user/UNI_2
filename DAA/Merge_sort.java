6f0148b61143fc955de3f31936921f5fdc0b7245
Satwik-user <satwik2400@gmail.com>
2026-05-01 00:16:58 +0000

    May 1, 2026, 12:16 AM

--- DAA/Merge_sort.java
+++ DAA/Merge_sort.java
@@ -4,7 +4,7 @@ class Merge_sort
     void merge(int arr[], int l, int m, int h)
     {
         int n1 = m - l + 1;
-        int n2 = r - m;
+        int n2 =  - m;
         int L[] = new int[n1];
         int H[] = new int[n2];
 
