6103977b39d59ed3652ec7a2d1fe502c0626f1de
Satwik-user <satwik2400@gmail.com>
2026-05-06 18:16:28 +0000

    May 6, 2026, 6:16 PM

--- DAA/multiply.c
+++ DAA/multiply.c
@@ -5,13 +5,15 @@ int main()
     printf("Enter row and column size of matrices:");
     scanf("%d %d %d %d", &n1, &m1, &n2, m2);
 
+    int A[n1][m1], B[n2][m2];
+
     printf("Enter 1st matrx:\n");
     for(int i=0; i<n1; i++)
     {
         for(int j=0; j<m1; j++)
         {
             printf("Enter element:");
-            
+            scanf("%d", A[i][j]);
         }
     }
     
