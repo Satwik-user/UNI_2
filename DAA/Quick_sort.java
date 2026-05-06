6397aba7a184dd56b6269643754528dbfcab618a
Satwik-user <satwik2400@gmail.com>
2026-05-06 16:06:06 +0000

    May 6, 2026, 4:06 PM

--- DAA/Quick_sort.java
+++ DAA/Quick_sort.java
@@ -44,7 +44,7 @@ class Quick_sort
         System.out.print("Enter size of array:");
         int n = sc.nextInt();
 
-
+        
         for(int i=0; i<n; i++)
         {
             System.out.print("Enter element:");
