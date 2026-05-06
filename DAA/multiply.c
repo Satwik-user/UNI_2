ea4aa10a449b38c0cc85a703910d28d98c53e43b
Satwik-user <satwik2400@gmail.com>
2026-05-06 18:27:36 +0000

    May 6, 2026, 6:27 PM

--- DAA/multiply.c
+++ DAA/multiply.c
@@ -41,8 +41,12 @@ int main()
 
     printf("Resultant Matrix:");
     for(int i=0; i<n1; i++)
-    for(int j=0; j<m2; j++)
-    
+    {
+        for(int j=0; j<m2; j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
