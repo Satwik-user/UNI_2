1ed8e8a4f9a69a884606da0afcc0d56b60fbfc8b
Satwik-user <satwik2400@gmail.com>
2026-05-06 18:13:21 +0000

    May 6, 2026, 6:13 PM

--- DAA/multiply.c
+++ DAA/multiply.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n1, m1, n2, m2;
     printf("Enter row and column size of matrices:");
+    scanf("%d %d %d %d", &n1, &m1, &n2, m2);
     
     
     return 0;
