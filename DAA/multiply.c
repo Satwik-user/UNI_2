30279b5211414f8b11eaa72f0bb95581bc86dfda
Satwik-user <satwik2400@gmail.com>
2026-05-06 18:14:55 +0000

    May 6, 2026, 6:14 PM

--- DAA/multiply.c
+++ DAA/multiply.c
@@ -6,7 +6,14 @@ int main()
     scanf("%d %d %d %d", &n1, &m1, &n2, m2);
 
     printf("Enter 1st matrx:\n");
-    for(int i=0; i<n1; U)
+    for(int i=0; i<n1; i++)
+    {
+        for(int j=0; j<m1; j++)
+        {
+            printf("Enter element:");
+            
+        }
+    }
     
     return 0;
 }
