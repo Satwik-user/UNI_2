aca663f48a99ce405bf64c8d897e219daf80f267
Satwik-user <satwik2400@gmail.com>
2026-05-06 15:48:25 +0000

    May 6, 2026, 3:48 PM

--- DAA/Quick_sort.java
+++ DAA/Quick_sort.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 class Quick_sort
 {
-    void swap(int arr[], int a, int b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
-\
+    void swap(i)
 }
